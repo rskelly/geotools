6ee72ac83ac1f00084d712733d296a29955702ba
rob <rob@dijital.ca>
2018-05-11 15:38:42 -0700

    Text reader.

--- include/reader.hpp
+++ include/reader.hpp
@@ -11,6 +11,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <unordered_map>
 
 #include <gdal_priv.h>
 
@@ -61,4 +62,45 @@ public:
 	int rows() const;
 	~GDALReader();
 };
+
+class px {
+	int c, r;
+	std::vector<double> values;
+	px(int c, int r) :
+		c(c), r(r) {}
+	px() : px(0, 0) {}
+};
+
+class TextReader : public Reader {
+private:
+	int m_cols;
+	int m_rows;
+	int m_bands;
+	std::unordered_map<long, px> m_pixels;
+	int m_col;
+	int m_row;
+	int m_bufSize;
+
+	std::map<int, int> m_bandMap;
+	int m_minWl; // These are scaled to avoid representation issues.
+	int m_maxWl;
+	int m_minIdx;
+	int m_maxIdx;
+
+public:
+	TextReader(const std::string& filename);
+	void setBufSize(int bufSize);
+	bool next(std::vector<double>& buf, int& col, int& row, int& cols, int& rows);
+	void setBandMap(std::map<int, int>& map);
+	void setBandMap(std::string& bandfile);
+	void setBandRange(double min, double max);
+	std::vector<double> getBands() const;
+	std::vector<double> getBandRange() const;
+	std::vector<int> getIndices() const;
+	int bands() const;
+	int cols() const;
+	int rows() const;
+	~TextReader();
+};
+
 #endif /* READER_HPP_ */
--- src/reader.cpp
+++ src/reader.cpp
@@ -144,3 +144,152 @@ int GDALReader::bands() const {
 GDALReader::~GDALReader() {
 	GDALClose(m_ds);
 }
+
+
+
+TextReader::TextReader(const std::string& filename) :
+	m_cols(0), m_rows(0), m_bands(0),
+	m_col(0), m_row(0),
+	m_bufSize(256),
+	m_minWl(0), m_maxWl(0),
+	m_minIdx(0), m_maxIdx(0) {
+
+
+	std::ifstream input(filename, std::ios::in);
+	std::string buf, item;
+	while(std::getline(input, buf)) {
+		if(buf[0] == ';') continue;
+
+		std::vector<std::string> fields;
+		while(std::getline(std::stringstream(item), buf, ' ')) {
+			if(!item.empty())
+				fields.push_back(item);
+		}
+
+		int col = atoi(fields[1].c_str());
+		int row = atoi(fields[2].c_str());
+		px& p = m_pixels[((long) col << 32) | row];
+		p.c = col;
+		p.r = row;
+		for(size_t i = 3; i < fields.size(); ++i)
+			p.values.push_back(atof(fields[i].c_str()));
+	}
+
+	m_bands = m_ds->GetRasterCount();
+	m_cols = m_ds->GetRasterXSize();
+	m_rows = m_ds->GetRasterYSize();
+
+	{
+		std::map<int, int> bandMap;
+		std::string name = "wavelength";
+		for(int i = 1; i <= m_bands; ++i) {
+			GDALRasterBand* band = m_ds->GetRasterBand(i);
+			std::string meta(band->GetMetadataItem(name.c_str()));
+			if(!meta.empty()) {
+				// The wavelength is scaled so that exact matches can occur.
+				int wl = (int) (atof(meta.c_str()) * WL_SCALE);
+				if(wl > 0) {
+					bandMap[wl] = i;
+				} else {
+					bandMap.clear();
+					std::cerr << "Failed to read band map from metadata." << std::endl;
+					break;
+				}
+			}
+		}
+		setBandMap(bandMap);
+	}
+}
+
+void TextReader::setBufSize(int bufSize) {
+	m_bufSize = bufSize;
+}
+
+bool TextReader::next(std::vector<double>& buf, int& col, int& row, int& cols, int& rows) {
+	if(m_col >= m_cols) {
+		m_row += m_bufSize;
+		m_col = 0;
+	}
+	if(m_row >= m_rows)
+		return false;
+
+	col = m_col;
+	row = m_row;
+	cols = std::min(m_bufSize, m_cols - m_col);
+	rows = std::min(m_bufSize, m_rows - m_row);
+
+	m_col += m_bufSize;
+
+	double* data = (double*) buf.data();
+	for(int i = m_minIdx; i <= m_maxIdx; ++i) {
+		GDALRasterBand* band = m_ds->GetRasterBand(i);
+		if(band->RasterIO(GF_Read, col, row, cols, rows,
+				(void*) (data + (i - m_minIdx) * m_bufSize * m_bufSize),
+				m_bufSize, m_bufSize, GDT_Float64, 0, 0, 0))
+			return false;
+	}
+	return true;
+}
+
+void TextReader::setBandMap(std::map<int, int>& map) {
+	m_bandMap = map;
+	m_minIdx = 0;
+	m_maxIdx = map.size() - 1;
+	m_minWl = std::next(m_bandMap.begin(), m_minIdx)->first;
+	m_maxWl = std::next(m_bandMap.begin(), m_maxIdx)->first;
+}
+
+void TextReader::setBandMap(std::string& bandfile) {
+	throw std::runtime_error("Not implemented.");
+}
+
+void TextReader::setBandRange(double min, double max) {
+	int mins = (int) (min * WL_SCALE);
+	int maxs = (int) (max * WL_SCALE);
+	for(auto it = m_bandMap.begin(); it != m_bandMap.end(); ++it) {
+		if(it->first <= mins) {
+			m_minWl = it->first;
+			m_minIdx = it->second;
+		}
+		if(it->first >= maxs) {
+			m_maxWl = it->first;
+			m_maxIdx = it->second;
+			break;
+		}
+	}
+}
+
+std::vector<double> TextReader::getBandRange() const {
+	return {(double) m_minWl / WL_SCALE, (double) m_maxWl / WL_SCALE};
+}
+
+std::vector<double> TextReader::getBands() const {
+	std::vector<double> bands;
+	if(m_maxIdx > 0 && m_maxIdx > m_minIdx) {
+		for(auto p = std::next(m_bandMap.begin(), m_minIdx - 1); p != std::next(m_bandMap.begin(), m_maxIdx); ++p)
+			bands.push_back((double) p->first / WL_SCALE);
+	} else {
+		for(const auto& p : m_bandMap)
+			bands.push_back((double) p.first / WL_SCALE);
+	}
+	return bands;
+}
+
+std::vector<int> TextReader::getIndices() const {
+	return {m_minIdx, m_maxIdx};
+}
+
+int TextReader::cols() const {
+	return m_cols;
+}
+
+int TextReader::rows() const {
+	return m_rows;
+}
+
+int TextReader::bands() const {
+	return m_maxIdx - m_minIdx + 1;
+}
+
+TextReader::~TextReader() {
+}
