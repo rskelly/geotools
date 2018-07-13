cc8384129f2c808a312014064422c71a9a8a55f8
rob <rob@dijital.ca>
2018-07-13 15:26:18 -0700

    Changed discrete kernel to a continuous one. Refactored.
    This is a working version.

--- include/convolver.hpp
+++ include/convolver.hpp
@@ -9,23 +9,326 @@
 #define INCLUDE_CONVOLVER_HPP_
 
 #include <string>
+#include <fstream>
 
+/**
+ * The kernel class holds the step values, plus the min
+ * and max wavelengths for the range.
+ */
+class Kernel {
+private:
+	double m_wl;
+	double m_fwhm;
+	double m_threshold;
+	double m_sigma;
+	double m_halfWidth;
+
+public:
+
+	/**
+	 * Create a kernel around the given wavelength with the given
+	 * full width at half maximum. The threshold gives the value
+	 * of the Gaussian beyond which the kernel is not applied.
+	 *
+	 * @param wl The centre wavelength.
+	 * @param fwhm The full width at half maximum.
+	 * @param threshold The minimum function value.
+	 */
+	Kernel(double wl, double fwhm, double threshold);
+
+	/**
+	 * Set the wavelength.
+	 *
+	 * @param wl The wavelength.
+	 */
+	void setWavelength(double wl);
+
+	/**
+	 * Get the wavelength.
+	 *
+	 * @return The wavelength.
+	 */
+	double wl() const;
+
+	/**
+	 * Set the full width at half maximum.
+	 *
+	 * @param fwhm The full width at half maximum.
+	 */
+	void setFWHM(double fwhm);
+
+	/**
+	 * Get the full width at half maximum.
+	 *
+	 * @return The full width at half maximum.
+	 */
+	double fwhm() const;
+
+	/**
+	 * Initialize the internal variables.
+	 */
+	void init();
+
+	/**
+	 * Returns the absolute distance from the centre wavelength, to the wavelength where
+	 * the function falls below the given threshold. This gives the wavelength
+	 * range to which the kernel can be applied.
+	 *
+	 * @param threshold The minimum y-value of the function.
+	 * @return The distance (in wavelength) from the centre of the
+	 * 		   curve to the point where it falls below the threshold.
+	 */
+	double halfWidth() const;
+
+	/**
+	 * Return the value of the Gaussian for the given wavelength.
+	 *
+	 * @param The target wavelength.
+	 * @return The Gaussian function output.
+	 */
+	double operator()(double wl0) const;
+
+	/**
+	 * Returns the threshold, the minimum value of the Gaussian
+	 * before it is ignored.
+	 *
+	 * @return The minimum Gaussian value.
+	 */
+	double threshold() const;
+
+};
+
+
+/**
+ * Contains the information about each band that will be used to build
+ * the convolution kernel.
+ */
+class BandProp {
+public:
+	int band;		///<! The 1-based band index.
+	double wl;		///<! The wavelength.
+	double fwhm;	///<! Full width at half maximum.
+
+	/**
+	 * Create a BandProp instance.
+	 *
+	 * @param band The 1-based band index.
+	 * @param wl The wavelength.
+	 * @param fwhm The full width of half maximum.
+	 */
+	BandProp(int band, double wl, double fwhm);
+};
+
+
+/**
+ * Represents a single band in the input/output spectra.
+ */
+class Band {
+public:
+	double wl;		///!< The wavelength.
+	double value;	///!< The value or intensity.
+
+	/**
+	 * Create a band.
+	 *
+	 * @param wl The wavelength.
+	 * @param value The value or intensity.
+	 */
+	Band(double wl, double value);
+
+	Band();
+};
+
+
+
+/**
+ * The spectrum object has a list of bands. When loaded from a file,
+ * loads the bands and stores the header properties.
+ *
+ * When the next() method is called, reads the next available spectrum,
+ * updates the bands and the date and timestamp.
+ */
+class Spectrum {
+private:
+	std::ifstream m_input;							///<! The input stream.
+	std::string m_buf;								///<! The current file buffer.
+	size_t m_count;									///<! The number of records in the file.
+
+public:
+	std::vector<Band> bands;						///<! A list of the bands. This changes as the file is read through.
+	std::map<std::string, std::string> properties;	///<! Properties read from the header block.
+	std::string date;								///<! The date of the current row.
+	long time;										///<! The timestamp of the current row.
+
+	/**
+	 * Load the data file and read the header information.
+	 * After load is called (and returns true), the next method must be
+	 * called to populate the bands list with data.
+	 *
+	 * @param filename A data file.
+	 * @return True if the file is loaded and has information in it.
+	 */
+	bool load(const std::string& filename);
+
+	/**
+	 * Return the number of spectra.
+	 */
+	size_t count() const;
+
+	/**
+	 * Advance the reader to the next line of data. This becomes the Spectrum's current state:
+	 * the list of Bands contains data from the current row.
+	 *
+	 * @return True if a row has been read, false if there were none left.
+	 */
+	bool next();
+
+	/**
+	 * Sets up the given Spectrum with the same structure and
+	 * wavelengths as the current one.
+	 *
+	 * @param spec A Spectrume.
+	 */
+	void setup(Spectrum& spec);
+
+	/**
+	 * Convolve using the given Kernel, writing the output to the given
+	 * Band.
+	 *
+	 * @param kernel A Kernel.
+	 * @param band A Band.
+	 */
+
+	void convolve(Kernel& kernel, Band& band);
+
+	/**
+	 * Set the values of all bands to zero.
+	 */
+	void reset();
+
+	/**
+	 * Write the header of an output spectrum, between the given
+	 * wavelengths.
+	 *
+	 * @param out An output stream.
+	 * @param minWl The minimum wavelength to print.
+	 * @param maxWl The maximum wavelength to print.
+	 */
+	void writeHeader(std::ostream& out, double minWl, double maxWl);
+
+	/**
+	 * Write the bands of an output spectrum, between the given
+	 * wavelengths.
+	 *
+	 * @param out An output stream.
+	 * @param minWl The minimum wavelength to print.
+	 * @param maxWl The maximum wavelength to print.
+	 */
+	void write(std::ostream& out, double minWl, double maxWl);
+
+};
+
+
+/**
+ * Read the convolution configuration file. The columns are
+ * band #, wavelength, full width at half maximum.
+ * Names are ignored; the order is important.
+ */
+class BandPropsReader {
+public:
+	std::map<int, BandProp> bandProps;	///<! The map of BandProp object.
+	double minWl;						///<! The minimum wavelength in the band definition file.
+	double maxWl;						///<! The maximum wavelength in the band definition file.
+
+	/**
+	 * Load the band definition file.
+	 *
+	 * @param filename The filename.
+	 */
+	void load(const std::string& filename);
+
+	/**
+	 * Returns a vector containing the list of band numbers.
+	 *
+	 * @return A vector containing the list of band numbers.
+	 */
+	std::vector<int> bands() const;
+
+	/**
+	 * Configure the given kernel using information for the given 1-based
+	 * band index. Throws a runtime exception if the band is not available.
+	 *
+	 * @param kernel The Kernel.
+	 * @param band The 1-based band index.
+	 */
+	void configureKernel(Kernel& kernel, int band);
+
+	/**
+	 * Configure the given Spectrum with the bands represented
+	 * by the band definition file.
+	 *
+	 * @param spec A Spectrum object.
+	 */
+	void configureSpectrum(Spectrum& spec);
+
+};
+
+
+
+/**
+ * Forward declaration.
+ */
 class Convolver;
 
+/**
+ * An interface that provides implementors with the ability to receive
+ * notifications from the Convolver.
+ */
 class ConvolverListener {
 public:
+
+	/**
+	 * Called when the convolution has started.
+	 *
+	 * @param A Convolver.
+	 */
 	virtual void started(Convolver*) = 0;
+
+	/**
+	 * Called when the convolution status has updated.
+	 *
+	 * @param A Convolver.
+	 */
 	virtual void update(Convolver*) = 0;
+
+	/**
+	 * Called when the convolution has stopped.
+	 *
+	 * @param A Convolver.
+	 */
 	virtual void stopped(Convolver*) = 0;
+
+	/**
+	 * Called when the convolution has finished.
+	 *
+	 * @param A Convolver.
+	 */
 	virtual void finished(Convolver*) = 0;
+
 	virtual ~ConvolverListener() {}
 };
 
+
+/**
+ * Performs the convolution of a spectral file using Gaussian kernels
+ * parameterized from a band definition file.
+ */
 class Convolver {
 private:
-	double m_progress;
+	double m_progress;	///<! The current progress, between 0 and 1.
 
 public:
+
 	/**
 	 * Run the convolver on the given files. The listener will receive callbacks.
 	 *
@@ -40,12 +343,9 @@ public:
 			bool& running);
 
 	/**
-	 * Cancel the run.
-	 */
-	void cancel();
-
-	/**
 	 * Return the progress as a double between 0 and 1.
+	 *
+	 * @return The progress as a double between 0 and 1.
 	 */
 	double progress() const;
 };
--- src/convolver.cpp
+++ src/convolver.cpp
@@ -12,10 +12,11 @@
 #include <sstream>
 #include <map>
 #include <string>
+#include <limits>
 
 #include "convolver.hpp"
 
-#define PI 3.1415926535
+constexpr double PI = 3.1415926535;
 
 /**
  * Compute the inverse of the Gaussian: retrieve the x that would give
@@ -26,7 +27,7 @@
  * @param x0 The mean x.
  */
 double invGaussian(double sigma, double y, double x0) {
-	return sigma * std::sqrt(-2.0 * std::log(y));
+	return sigma * std::sqrt(-2.0 * std::log(y * 2.0 * PI)) + x0;
 }
 
 /**
@@ -38,353 +39,309 @@ double invGaussian(double sigma, double y, double x0) {
  * @param x0 The mean x.
  */
 double gaussian(double sigma, double x, double x0) {
-	return std::exp(-0.5 * std::pow((x - x0) / sigma, 2.0));
+	return  1.0 / std::sqrt(2.0 * PI) * std::exp(-0.5 * std::pow((x - x0) / sigma, 2.0));
 }
 
-/**
- * The kernel class holds the step values, plus the min
- * and max wavelengths for the range.
- */
-class Kernel {
-public:
-	double wl;
-	double min;
-	double max;
-	std::vector<double> kernel;
 
-	Kernel(double wl, double min, double max) :
-		wl(wl), min(min), max(max) {}
+Kernel::Kernel(double wl, double fwhm, double threshold) :
+	m_wl(wl),
+	m_fwhm(fwhm),
+	m_threshold(threshold) {
+	init();
+}
 
-	Kernel() : Kernel(0, 0, 0) {}
+void Kernel::setWavelength(double wl) {
+	m_wl = wl;
+	init();
+}
 
-	void resize(int size) {
-		kernel.resize(size);
-		kernel[0] = 0;
-		kernel[size - 1] = 0;
+double Kernel::wl() const {
+	return m_wl;
+}
+
+void Kernel::setFWHM(double fwhm) {
+	m_fwhm = fwhm;
+	init();
+}
+
+double Kernel::fwhm() const {
+	return m_fwhm;
+}
+
+void Kernel::init() {
+	m_sigma = ((0.5 * m_fwhm) / (2.0 * std::sqrt(2.0 * std::log(2.0))));
+	double w = invGaussian(m_sigma, m_threshold, m_wl);
+	m_halfWidth = std::abs(m_wl - w);
+}
+
+double Kernel::halfWidth() const {
+	return m_halfWidth;
+}
+
+double Kernel::operator()(double wl0) const {
+	return gaussian(m_sigma, wl0, m_wl);
+}
+
+double Kernel::threshold() const {
+	return m_threshold;
+}
+
+
+
+BandProp::BandProp(int band, double wl, double fwhm) :
+		band(band), wl(wl), fwhm(fwhm) {
+}
+
+
+
+Band::Band(double wl, double value) :
+			wl(wl), value(value) {
+}
+
+Band::Band() : Band(0, 0) {
+}
+
+
+
+bool Spectrum::load(const std::string& filename) {
+	// Clear any existing bands list.
+	bands.clear();
+	// Open the input file for reading.
+	m_input.open(filename, std::ios::in);
+
+	// Try to skip the header. If it doesn't work quit.
+	if(!std::getline(m_input, m_buf))
+		return false;
+
+	std::string buf;		// A temporary buffer.
+	bool header = false; 	// False when the band wl header hasn't been read yet.
+
+	// Run over the rows.
+	while(std::getline(m_input, m_buf)) {
+		if(!header && m_buf.find(':') < std::string::npos) {
+			// Process the colon-delimited headers for properties.
+			std::stringstream ss(m_buf);
+			std::getline(ss, buf, ':');
+			std::getline(ss, m_buf);
+			properties[buf] = m_buf;
+		} else if(m_buf.find(">>>") < std::string::npos) {
+			// Skip the divider.
+			continue;
+		} else if(!header) {
+			// Parse the wavelengths out of the header section. The first two columns (date, time) are empty.
+			std::stringstream ss(m_buf);
+			std::getline(ss, buf, '\t');
+			std::getline(ss, buf, '\t');
+			while(std::getline(ss, buf, '\t'))
+				bands.emplace_back(atof(buf.c_str()), 0);
+			header = true;
+		} else {
+			// We're at the start of data. Get the position, and count the remaining rows.
+			size_t pos = m_input.tellg();
+			std::string dummy;
+			m_count = 1;
+			while(std::getline(m_input, dummy))
+				++m_count;
+
+			// Close and open the file, and seek back to where we left off.
+			// The first row is already in the buffer.
+			m_input.close();
+			m_input.open(filename, std::ios::in);
+			if(!m_input.seekg(pos).good())
+				return false;
+			break;
+		}
+	}
+	return true;
+}
+
+size_t Spectrum::count() const {
+	return m_count;
+}
+
+bool Spectrum::next() {
+	// No buffer was read on the previous read. We're done.
+	if(m_buf.empty())
+		return false;
+	// Read the date, timestamp and values out of the line.
+	std::stringstream ss(m_buf);
+	std::string part;
+	std::getline(ss, date, '\t');
+	std::getline(ss, part, '\t');
+	time = std::strtol(part.c_str(), nullptr, 10);
+	size_t i = 0;
+	while(std::getline(ss, part, '\t'))
+		bands[i++].value = std::strtod(part.c_str(), nullptr);
+	// Read the next buffer. If it fails, we'll find out on the next call to next.
+	std::getline(m_input, m_buf);
+	return true;
+}
+
+void Spectrum::setup(Spectrum& spec) {
+	// Instantiate the bands on the new spectrum.
+	for(const Band& b : bands)
+		spec.bands.emplace_back(b.wl, 0);
+}
+
+void Spectrum::convolve(Kernel& kernel, Band& band) {
+	double min = band.wl - kernel.halfWidth();
+	double max = band.wl + kernel.halfWidth();
+	// Find the index nearest the centre of the kernel.
+	size_t idx0 = 0;
+	size_t idx1 = bands.size();
+	for(size_t i = 0; i < bands.size(); ++i) {
+		if(min > bands[i].wl)
+			idx0 = i;
+		else break;
+	}
+	for(size_t i = idx0; i < bands.size(); ++i) {
+		if(max < bands[i].wl) {
+			idx1 = i;
+			break;
+		}
 	}
 
-	double& operator[](size_t idx) {
-		return kernel[idx];
+	// Convolve the bands.
+	for(size_t i = idx0; i < idx1; ++i) {
+		double v = kernel(bands[i].wl);
+		band.value += bands[i].value * v;
 	}
+}
 
-	/**
-	 * Normalize the kernal so that its elements sum to 1.
-	 */
-	void normalize() {
-		double s = 0;
-		for(double d : kernel)
-			s += d;
-		for(double& d : kernel)
-			d /= s;
+void Spectrum::reset() {
+	for(Band& b : bands)
+		b.value = 0;
+}
+
+void Spectrum::writeHeader(std::ostream& out, double minWl, double maxWl) {
+	out << "date,timestamp";
+	for(const Band& b : bands) {
+		if(b.wl >= minWl && b.wl <= maxWl)
+			out << "," << b.wl;
 	}
+	out << "\n";
+}
 
-	/**
-	 * Build a 1-dimensional kernel contaning the discretized Gaussian with
-	 * size elements. The Gaussian is parameterized by its mean (the bandwidth) and
-	 * standard deviation, which is computed from the full width at half magnitude (FWHM)
-	 * using the function 2*sqrt(2*ln(2)) * sigma (https://en.wikipedia.org/wiki/Full_width_at_half_maximum).
-	 *
-	 * The kernel is normalized to sum to 1.
-	 *
-	 * @param size The number of elements in the kernel (one element added if even, so that there's a zero at each end and the peak in the middle).
-	 * @param wl The wavelength in whatever units; must be uniform for all sources.
-	 * @param fwhm Full width at half maximum.
-	 * @param threshold The minimum value of the function, which will delineate the "ends" at index 0, and size.
-	 */
-	void build(int size, double wl, double fwhm, double threshold) {
-		if(size % 2 == 0) ++size;
-		// Half-width at half maximum.
-		double hwhm = fwhm * 0.5;
-		// Recover the std. dev from the hwhm.
-		double sigma = hwhm / (2.0 * std::sqrt(2.0 * std::log(2.0)));
-		// Get the distance from wl for the threshold value.
-		double inv = invGaussian(sigma, threshold, wl);
-		// Find the bounds for the curve, and the step width.
-		this->wl = wl;
-		min = wl - inv;
-		max = wl + inv;
-		double step = (max - min) / (size - 1);
-		// Compute the steps.
-		kernel.resize(size);
-		for(int i = 0; i < size; ++i)
-			kernel[i] = gaussian(sigma, min + step * i, wl);
-		// Normalize the kernel.
-		normalize();
+void Spectrum::write(std::ostream& out, double minWl, double maxWl) {
+	out << date << "," << time;
+	for(const Band& b : bands) {
+		if(b.wl >= minWl && b.wl <= maxWl)
+			out << "," << b.value;
 	}
-};
+	out << "\n";
+}
 
 
-/**
- * Contains the information about each band that will be used to build
- * the convolution kernel.
- */
-class BandProp {
-public:
+void BandPropsReader::load(const std::string& filename) {
+	// Set the initial limits. These will be refined.
+	minWl = std::numeric_limits<double>::max();
+	maxWl = std::numeric_limits<double>::lowest();
+	// Open file, initialize buffers.
+	std::ifstream input(filename, std::ios::in);
+	std::string buf, part;
+	// Try to skip the header. If it doesn't work quit.
+	if(!std::getline(input, buf))
+		return;
+	// Run over the rows.
 	int band;
-	double wl;
-	double fwhm;
-	BandProp(int band, double wl, double fwhm) :
-		band(band), wl(wl), fwhm(fwhm) {}
-};
-
-class Band {
-public:
-	double wl;
-	double value;
-	Band(double wl, double value) :
-		wl(wl), value(value) {}
-	Band() : Band(0, 0) {}
-};
-
-/**
- * The spectrum object has a list of bands. When loaded from a file,
- * loads the bands and stores the header properties.
- *
- * When the next() method is called, reads the next available spectrum,
- * updates the bands and the date and timestamp.
- */
-class Spectrum {
-private:
-	std::ifstream m_input;
-	std::string m_buf;
-	size_t m_count;
-
-public:
-	std::vector<Band> bands;						///<! A list of the bands. This changes as the file is read through.
-	std::map<std::string, std::string> properties;	///<! Properties read from the header block.
-	std::string date;								///<! The date of the current row.
-	long time;										///<! The timestamp of the current row.
-
-	/**
-	 * Load the data file and read the header information.
-	 * After load is called (and returns true), the next method must be
-	 * called to populate the bands list with data.
-	 *
-	 * @param filename A data file.
-	 * @return True if the file is loaded and has information in it.
-	 */
-	bool load(const std::string& filename) {
-		bands.clear();
-		m_input.open(filename, std::ios::in);
-		std::string part;
-		// Try to skip the header. If it doesn't work quit.
-		if(!std::getline(m_input, m_buf))
-			return false;
-		// Run over the rows.
-		bool header = false; // The band wl header hasn't been read yet.
-		while(std::getline(m_input, m_buf)) {
-			//count = (pos = buf.find(',')) == std::string::npos ? buf.size() : buf.size() - pos;
-			//fwhm = atof(buf.substr(0, count).c_str()); // May or may not be a comma at the end.
-			if(!header && m_buf.find(':') < std::string::npos) {
-				// Process the colon-delimited headers for properties.
-				std::stringstream ss(m_buf);
-				std::getline(ss, part, ':');
-				std::getline(ss, m_buf);
-				properties[part] = m_buf;
-			} else if(m_buf.find(">>>") < std::string::npos) {
-				// Skip the divider.
-				continue;
-			} else if(!header) {
-				// Parse the wavelengths out of the header section. The first two columns (date, time) are empty.
-				std::stringstream ss(m_buf);
-				std::getline(ss, part, '\t');
-				std::getline(ss, part, '\t');
-				while(std::getline(ss, part, '\t'))
-					bands.emplace_back(atof(part.c_str()), 0);
-				header = true;
-			} else {
-				// We're at the start of data.
-				size_t pos = m_input.tellg();
-				std::string dummy;
-				m_count = 1;
-				while(std::getline(m_input, dummy))
-					++m_count;
-				m_input.close();
-				m_input.open(filename, std::ios::in);
-				if(!m_input.seekg(pos).good())
-					return false;
-				break;
-			}
-		}
-		return true;
+	double wl, fwhm;
+	size_t pos, count;
+	while(std::getline(input, buf)) {
+		std::stringstream ss(buf);
+		std::getline(ss, buf, ',');
+		band = std::strtol(buf.c_str(), nullptr, 10);
+		std::getline(ss, buf, ',');
+		wl = std::strtod(buf.c_str(), nullptr);
+		std::getline(ss, buf);
+		count = (pos = buf.find(',')) == std::string::npos ? buf.size() : buf.size() - pos;
+		fwhm = std::strtod(buf.substr(0, count).c_str(), nullptr); // May or may not be a comma at the end.
+		bandProps.emplace(std::piecewise_construct, std::forward_as_tuple(band), std::forward_as_tuple(band, wl, fwhm));
+		if(wl < minWl) minWl = wl;
+		if(wl > maxWl) maxWl = wl;
 	}
+}
 
-	/**
-	 * Return the number of spectra.
-	 */
-	size_t count() const {
-		return m_count;
-	}
+std::vector<int> BandPropsReader::bands() const {
+	std::vector<int> bands;
+	for(const auto& p : bandProps)
+		bands.push_back(p.first);
+	return bands;
+}
 
-	/**
-	 * Advance the reader to the next line of data. This becomes the Spectrum's current state:
-	 * the list of Bands contains data from the current row.
-	 *
-	 * @return True if a row has been read, false if there were none left.
-	 */
-	bool next() {
-		if(m_buf.empty())
-			return false;
+void BandPropsReader::configureKernel(Kernel& kernel, int band) {
+	// If the band is not found, raise an error.
+	if(bandProps.find(band) == bandProps.end())
+		throw std::runtime_error("Band not found: " + std::to_string(band));
+	// Get the band and set the properties.
+	const BandProp& p = bandProps.at(band);
+	kernel.setWavelength(p.wl);
+	kernel.setFWHM(p.fwhm);
+}
 
-		std::stringstream ss(m_buf);
-		std::string part;
-		std::getline(ss, date, '\t');
-		std::getline(ss, part, '\t');
-		time = std::strtol(part.c_str(), nullptr, 10);
-		size_t i = 0;
-		while(std::getline(ss, part, '\t'))
-			bands[i++].value = std::strtod(part.c_str(), nullptr);
+void BandPropsReader::configureSpectrum(Spectrum& spec) {
+	// Resize the spectrum to the number of bands in the configuration.
+	spec.bands.resize(bandProps.size());
+	// Set all the wavelengths on the bands.
+	size_t i = 0;
+	for(const auto& p : bandProps)
+		spec.bands[i++].wl = p.second.wl;
+}
 
-		std::getline(m_input, m_buf);
-		return true;
-	}
 
-	void setup(Spectrum& spec) {
-		// Instantiate the bands on the new spectrum.
-		for(const Band& b : bands)
-			spec.bands.emplace_back(b.wl, 0);
-	}
-
-	void convolve(Kernel& kernel, Band& band) {
-		// Find the index nearest the centre of the kernel.
-		size_t idx = 0;
-		for(size_t i = 0; i < bands.size(); ++i) {
-			if(kernel.min < bands[i].wl) break;
-			idx = i;
-		}
-		// Convolve the bands.
-		for(size_t i = 0; i < kernel.kernel.size(); ++i)
-			band.value += bands[i + idx].value * kernel[i];
-	}
-
-	void reset() {
-		for(Band& b : bands)
-			b.value = 0;
-	}
-
-	void writeHeader(std::ostream& out, double minWl, double maxWl) {
-		out << "date,timestamp";
-		for(const Band& b : bands) {
-			if(b.wl >= minWl && b.wl <= maxWl)
-				out << "," << b.wl;
-		}
-		out << "\n";
-	}
-
-	void write(std::ostream& out, double minWl, double maxWl) {
-		out << date << "," << time;
-		for(const Band& b : bands) {
-			if(b.wl >= minWl && b.wl <= maxWl)
-				out << "," << b.value;
-		}
-		out << "\n";
-	}
-
-};
-
-/**
- * Read the convolution configuration file. The columns are
- * band #, wavelength, full width at half maximum.
- * Names are ignored; the order is important.
- */
-class BandPropsReader {
-public:
-	std::map<int, BandProp> bandProps;
-	double minWl;
-	double maxWl;
-
-	void load(const std::string& filename) {
-		minWl = std::numeric_limits<double>::max();
-		maxWl = std::numeric_limits<double>::lowest();
-
-		std::ifstream input(filename, std::ios::in);
-		std::string buf, part;
-		// Try to skip the header. If it doesn't work quit.
-		if(!std::getline(input, buf))
-			return;
-		// Run over the rows.
-		int band;
-		double wl, fwhm;
-		size_t pos, count;
-		while(std::getline(input, buf)) {
-			std::stringstream ss(buf);
-			std::getline(ss, buf, ',');
-			band = std::strtol(buf.c_str(), nullptr, 10);
-			std::getline(ss, buf, ',');
-			wl = std::strtod(buf.c_str(), nullptr);
-			std::getline(ss, buf);
-			count = (pos = buf.find(',')) == std::string::npos ? buf.size() : buf.size() - pos;
-			fwhm = std::strtod(buf.substr(0, count).c_str(), nullptr); // May or may not be a comma at the end.
-			bandProps.emplace(std::piecewise_construct, std::forward_as_tuple(band), std::forward_as_tuple(band, wl, fwhm));
-			if(wl < minWl) minWl = wl;
-			if(wl > maxWl) maxWl = wl;
-		}
-	}
-
-	std::vector<int> bands() const {
-		std::vector<int> bands;
-		for(const auto& p : bandProps)
-			bands.push_back(p.first);
-		return bands;
-	}
-
-	void configureKernel(Kernel& kernel, int band, int size, double threshold) {
-		if(bandProps.find(band) == bandProps.end())
-			throw std::runtime_error("Band not found: " + std::to_string(band));
-		const BandProp& p = bandProps.at(band);
-		kernel.build(size, p.wl, p.fwhm, threshold);
-	}
-
-	void configureSpectrum(Spectrum& spec) {
-		spec.bands.resize(bandProps.size());
-		size_t i = 0;
-		for(const auto& p : bandProps)
-			spec.bands[i++].wl = p.second.wl;
-	}
-};
 
 void Convolver::run(ConvolverListener& listener,
 		const std::string& bandDef, const std::string& spectra, const std::string& output,
 		bool& running) {
 
+	// Notify a listener.
 	listener.started(this);
 
-	Kernel kernel;
+	// Configure the kernel; only the threshold value available at this time.
+	Kernel kernel(0, 0, 0.0001);
+
+	// Load the band properties.
 	BandPropsReader rdr;
 	rdr.load(bandDef);
 
+	// Load the spectrum.
 	Spectrum spec;
 	spec.load(spectra);
 
+	// Configure the output (convolved) spectrum.
 	Spectrum out;
 	rdr.configureSpectrum(out);
 
+	// Open the output file.
 	std::ofstream outstr(output, std::ios::out);
-	size_t complete = 0, count = spec.count();
+
+	// Run the convolution record-by-record.
+	size_t complete = 0, count = spec.count(); // Status counters.
 	bool header = false;
 	while(running && spec.next()) {
 		out.reset();
 		out.date = spec.date;
 		out.time = spec.time;
 		for(int b : rdr.bands()) {
-			rdr.configureKernel(kernel, b, 40, 0.001);
+			// For each band definition, configure the kernel.
+			rdr.configureKernel(kernel, b);
+			// Convolve the spectrum onto the given band.
 			spec.convolve(kernel, out.bands[b - 1]);
 			if(!running) break;
 		}
 		if(!header) {
+			// Write the header if this is the first iteration.
 			out.writeHeader(outstr, rdr.minWl, rdr.maxWl);
 			header = true;
 		}
+		// Write the record.
 		out.write(outstr, rdr.minWl, rdr.maxWl);
+		// Update progress.
 		m_progress = (double) complete++ / count;
 		listener.update(this);
 		if(!running) break;
 	}
+	// Notify listener of completion.
 	listener.finished(this);
-}
-
-void Convolver::cancel() {
-
 }
 
 double Convolver::progress() const {
