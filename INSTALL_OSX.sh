66a2b4b7d33c8b1ade3059bd2a98953c10cec6eb
process <process@baker.local>
2019-03-21 15:57:25 -0700

    Hacks to work on OSX. Questionable.

--- INSTALL_OSX.sh
+++ INSTALL_OSX.sh
@@ -1,16 +1,35 @@
 #!/bin/sh
 
+export PATH="/usr/local/opt/qt/bin:$PATH"
+sudo chmod +t /private/tmp
+sudo chown -R "$(whoami)":admin /usr/local
+
 # Must have homebrew installed. 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! [ -x "$(command -v brew)" ]; then
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+fi;
 
 # Install some necessaries.
-sudo brew install cmake
-sudo brew install geos
-sudo brew install gdal
-sudo brew install qt5
-sudo brew link geos
-sudo brew link gdal
-sudo brew link qt5
+brew update && brew upgrade
+
+brew install cmake
+brew install geos
+brew install gdal
+brew install qt5
+brew install eigen
+brew install ccache
+brew upgrade cmake
+brew upgrade geos
+brew upgrade gdal
+brew upgrade qt5
+brew upgrade eigen
+brew upgrade ccache
+brew unlink cmake && brew link cmake --force
+brew unlink geos && brew link geos --force
+brew unlink gdal && brew link gdal --force
+brew unlink qt && brew link qt5 --force
+brew unlink eigen && brew link eigen --force
+brew unlink ccache && brew link ccache --force
 
 # Build the software
 mkdir -p build
