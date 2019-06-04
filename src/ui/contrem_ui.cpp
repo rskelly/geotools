e7dcce388d830673825265a08d6e60e26dc3ca54
rob <rob@dijital.ca>
2019-06-03 18:05:17 -0700

    Working on contrem.

--- src/ui/contrem_ui.cpp
+++ src/ui/contrem_ui.cpp
@@ -12,16 +12,133 @@
 #include <QtGui/QDesktopServices>
 #include <QtWidgets/QMessageBox>
 
+#include <gdal_priv.h>
+
 #include "contrem_ui.hpp"
 #include "contrem.hpp"
 
+namespace {
+
+	constexpr const char* LAST_ROI = "lastROI";
+	constexpr const char* LAST_SPECTRA = "lastSpectra";
+	constexpr const char* LAST_INPUT_START_BAND = "lastInputStartBand";
+	constexpr const char* LAST_INPUT_END_BAND = "lastInputEndBand";
+	constexpr const char* LAST_INPUT_HAS_HEADER = "lastInputHasHeader";
+	constexpr const char* LAST_INPUT_START_COL = "lastInputStartCol";
+	constexpr const char* LAST_INPUT_END_COL = "lastInputEndCol";
+	constexpr const char* LAST_OUTPUT = "lastOutput";
+	constexpr const char* LAST_OUTPUT_TYPE = "lastOutputType";
+	constexpr const char* LAST_WL_COL = "lastWLCol";
+	constexpr const char* LAST_WL_BAND_COL = "lastWLBandCol";
+	constexpr const char* LAST_WL_HAS_HEADER = "lastWLHasHeader";
+	constexpr const char* LAST_MIN_WL = "lastMinWl";
+	constexpr const char* LAST_MAX_WL = "lastMaxWL";
+	constexpr const char* LAST_BUFFER = "lastBuffer";
+	constexpr const char* LAST_THREADS = "lastThreads";
+	constexpr const char* LAST_DIR = "lastDir";
+
+	enum FileType {
+		GTiff,
+		ENVI,
+		ROI,
+		SHP,
+		CSV,
+		Unknown
+	};
+
+	constexpr std::array<FileType, 3> INPUT_TYPES = {GTiff, ENVI, CSV};		///<! Allowed input types for spectral data.
+	constexpr std::array<FileType, 3> OUTPUT_TYPES = {GTiff, ENVI, CSV};	///<! Allowed output types for results.
+	constexpr std::array<FileType, 4> ROI_TYPES = {GTiff, ENVI, SHP, ROI};	///<! Allowed mask/ROI file types.
+
+	FileType getFileType(const std::string& filename) {
+		std::string ext;
+		{
+			size_t p = filename.find('.');
+			if(p < std::string::npos) {
+				std::string ext0 = filename.substr();
+				std::transform(ext0.begin(), ext0.end(), ext.begin(), ::tolower);
+			}
+		}
+		if(ext == ".csv") {
+			return CSV;
+		} else if(ext == ".roi") {
+			return ROI;
+		} else {
+			GDALAllRegister();
+			GDALDataset* ds = static_cast<GDALDataset*>(GDALOpen(filename.c_str(), GA_ReadOnly));
+			if(ds) {
+				std::string drv(ds->GetDriverName());
+				if(drv == "GTiff") {
+					GDALClose(ds);
+					return GTiff;
+				} else if(drv == "ENVI") {
+					GDALClose(ds);
+					return ENVI;
+				}
+			}
+		}
+		return Unknown;
+	}
+
+	void trun(ContremListener* form, Contrem* contrem, Reader* reader) {
+		contrem->run(form, reader);
+	}
+
+	/**
+	 * Return a map containing pairs where the int is the 1-based band index,
+	 * and the float is the wavelength. Attempts to load from raster metadata
+	 * or table header. If these fail, will attempt to load from first column
+	 * of presumably transposed table.
+	 */
+	std::map<int, double> loadWavelengths(const std::string& filename) {
+		std::map<int, double> map;
+		switch(getFileType(filename)) {
+		case GTiff:
+		case ENVI:
+			{
+				GDALReader rdr(filename);
+				for(const auto& it : rdr.getBandMap())
+					map[it.first] = it.second / WL_SCALE;
+			}
+			break;
+		case CSV:
+			{
+				CSVReader rdr(filename);
+				if(true)
+					rdr.transpose();
+				std::vector<double> row;
+				std::map<int, int> map;
+				for(size_t i = 0; i < row.size(); ++i) {
+					if(!std::isnan(row[i]))
+						map[i] = row[i];
+				}
+			}
+			break;
+		case SHP:
+		case ROI:
+		default:
+			throw std::runtime_error("Invalid file type: " + filename);
+		}
+		return map;
+	}
+
+	std::string fileTypeAsString(FileType type) {
+		switch(type) {
+		case GTiff: return "GTiff";
+		case ENVI: return "ENVI";
+		case ROI: return "ENVI ROI";
+		case SHP: return "Shapefile";
+		case CSV: return "CSV";
+		default: return "";
+		}
+	}
+}
+
+
 ContremForm::ContremForm(Contrem* contrem, QApplication* app) :
-	m_header(false),
-	m_wlCol(0),
-	m_bandCol(0),
+	m_inputHasHeader(false),
 	m_minWl(0),
 	m_maxWl(0),
-	m_popStats(false),
 	m_buffer(256),
 	m_threads(1),
 	m_contrem(contrem),
@@ -36,23 +153,31 @@ void ContremForm::setupUi(QDialog* form) {
 	m_form = form;
 	progressBar->setValue(0);
 
-	QStringList drivers;
-	drivers << "ENVI" << "GTiff";
-	cboDriver->addItems(drivers);
+	QStringList outputTypes;
+	outputTypes << "";
+	for(FileType type : OUTPUT_TYPES)
+		outputTypes << fileTypeAsString(type).c_str();
+	cboOutputType->addItems(outputTypes);
 
-	connect(txtROI, SIGNAL(textChanged(QString)), this, SLOT(txtROIChanged(QString)));
-	connect(txtWavelength, SIGNAL(textChanged(QString)), this, SLOT(txtWLChanged(QString)));
-	connect(txtSpectra, SIGNAL(textChanged(QString)), this, SLOT(txtSpectraChanged(QString)));
-	connect(txtOutput, SIGNAL(textChanged(QString)), this, SLOT(txtOutputChanged(QString)));
-	connect(chkHeader, SIGNAL(toggled(bool)), this, SLOT(chkHeaderChanged(bool)));
-	connect(spnWLCol, SIGNAL(valueChanged(int)), this, SLOT(spnWLColChanged(int)));
-	connect(spnBandCol, SIGNAL(valueChanged(int)), this, SLOT(spnBandColChanged(int)));
-	connect(cboDriver, SIGNAL(currentTextChanged(QString)), this, SLOT(cboDriverChanged(QString)));
-	connect(spnMinWL, SIGNAL(valueChanged(double)), this, SLOT(spnMinWLChanged(double)));
-	connect(spnMaxWL, SIGNAL(valueChanged(double)), this, SLOT(spnMaxWLChanged(double)));
-	connect(rdoPopulation, SIGNAL(toggled(bool)), this, SLOT(rdoPopStatsChanged(bool)));
+	QStringList spectraTypes;
+	outputTypes << "";
+	for(FileType type : INPUT_TYPES)
+		spectraTypes << fileTypeAsString(type).c_str();
+	cboSpectraType->addItems(spectraTypes);
+
+	QStringList roiTypes;
+	roiTypes << "";
+	for(FileType type : ROI_TYPES)
+		roiTypes << fileTypeAsString(type).c_str();
+	cboROIType->addItems(roiTypes);
+
+	connect(txtROIFile, SIGNAL(textChanged(QString)), this, SLOT(txtROIFileChanged(QString)));
+	connect(cboROIType, SIGNAL(currentTextChanged(QString)), this, SLOT(cboROITypeChanged(QString)));
+	connect(txtSpectraFile, SIGNAL(textChanged(QString)), this, SLOT(txtSpectraFileChanged(QString)));
+	connect(cboSpectraType, SIGNAL(currentTextChanged(QString)), this, SLOT(cboSpectraTypeChanged(QString)));
+	connect(txtOutputFile, SIGNAL(textChanged(QString)), this, SLOT(txtOutputFileChanged(QString)));
+	connect(cboOutputType, SIGNAL(currentTextChanged(QString)), this, SLOT(cboOutputTypeChanged(QString)));
 	connect(btnROI, SIGNAL(clicked()), this, SLOT(btnROIClicked()));
-	connect(btnWavelength, SIGNAL(clicked()), this, SLOT(btnWLClicked()));
 	connect(btnSpectra, SIGNAL(clicked()), this, SLOT(btnSpectraClicked()));
 	connect(btnOutput, SIGNAL(clicked()), this, SLOT(btnOutputClicked()));
 	connect(btnRun, SIGNAL(clicked()), this, SLOT(btnRunClicked()));
@@ -65,33 +190,21 @@ void ContremForm::setupUi(QDialog* form) {
 	connect(this, SIGNAL(update(Contrem*)), this, SLOT(convUpdate(Contrem*)));
 	connect(this, SIGNAL(finished(Contrem*)), this, SLOT(convFinished(Contrem*)));
 
-	m_roiFile = m_settings.value("lastROI", "").toString().toStdString();
-	m_wlMapFile = m_settings.value("lastWL", "").toString().toStdString();
-	m_spectraFile = m_settings.value("lastSpectra", "").toString().toStdString();
-	m_outputFile = m_settings.value("lastOutput", "").toString().toStdString();
-	m_outputDriver = m_settings.value("lastDriver", "ENVI").toString().toStdString();
-	m_wlCol = m_settings.value("lastWLCol", 0).toInt();
-	m_bandCol = m_settings.value("lastBandCol", 0).toInt();
-	m_header = m_settings.value("lastHeader", false).toBool();
-	m_minWl = m_settings.value("lastMinWL", 0).toDouble();
-	m_maxWl = m_settings.value("lastMaxWL", 0).toDouble();
-	m_popStats = m_settings.value("lastPopStats", true).toBool();
-	m_buffer = m_settings.value("lastBuffer", 256).toInt();
-	m_threads = m_settings.value("lastThreads", 1).toInt();
+	m_roiFile = m_settings.value(LAST_ROI, "").toString().toStdString();
+	m_spectraFile = m_settings.value(LAST_SPECTRA, "").toString().toStdString();
+	m_inputHasHeader = m_settings.value(LAST_INPUT_HAS_HEADER, false).toBool();
+	m_outputFile = m_settings.value(LAST_OUTPUT, "").toString().toStdString();
+	m_outputType = m_settings.value(LAST_OUTPUT_TYPE, "ENVI").toString().toStdString();
+	m_minWl = m_settings.value(LAST_MIN_WL, 0).toDouble();
+	m_maxWl = m_settings.value(LAST_MAX_WL, 0).toDouble();
+	m_buffer = m_settings.value(LAST_BUFFER, 256).toInt();
+	m_threads = m_settings.value(LAST_THREADS, 1).toInt();
 
-	txtROI->setText(QString(m_roiFile.c_str()));
-	txtWavelength->setText(QString(m_wlMapFile.c_str()));
-	txtSpectra->setText(QString(m_spectraFile.c_str()));
-	txtOutput->setText(QString(m_outputFile.c_str()));
-	cboDriver->setCurrentText(QString(m_outputDriver.c_str()));
-	spnWLCol->setValue(m_wlCol);
-	spnBandCol->setValue(m_bandCol);
-	chkHeader->setChecked(m_header);
-	spnMinWL->setValue(m_minWl);
-	spnMaxWL->setValue(m_maxWl);
-	rdoPopulation->setChecked(m_popStats);
-	spnBuffer->setValue(m_buffer);
-	spnThreads->setValue(m_threads);
+	txtROIFile->setText(QString(m_roiFile.c_str()));
+	txtSpectraFile->setText(QString(m_spectraFile.c_str()));
+	txtOutputFile->setText(QString(m_outputFile.c_str()));
+	cboOutputType->setCurrentText(QString(m_outputType.c_str()));
+	chkInputHasHeader->setChecked(m_inputHasHeader);
 }
 
 void ContremForm::checkRun() {
@@ -105,164 +218,114 @@ void ContremForm::checkRun() {
 	btnRun->setEnabled(true);
 }
 
-void ContremForm::chkHeaderChanged(bool value) {
-	m_header = value;
-	m_settings.setValue("lastHeader", value);
-	checkRun();
+void ContremForm::updateROIType() {
+	std::string fileType = fileTypeAsString(getFileType(m_roiFile));
+	cboROIType->setCurrentText(fileType.c_str());
 }
 
-void ContremForm::rdoPopStatsChanged(bool value) {
-	m_popStats = value;
-	m_settings.setValue("lastPopStats", value);
-	checkRun();
-}
-
-void ContremForm::spnThreadsChanged(int value) {
-	m_threads = value;
-	m_settings.setValue("lastThreads", value);
-	checkRun();
-}
-
-void ContremForm::spnBufferChanged(int value) {
-	m_buffer = value;
-	m_settings.setValue("lastValue", value);
-	checkRun();
-}
-
-void ContremForm::cboDriverChanged(QString value) {
-	m_outputDriver = value.toStdString();
-	m_settings.setValue("lastDriver", value);
-	checkRun();
-}
-
-void ContremForm::txtROIChanged(QString filename) {
+void ContremForm::txtROIFileChanged(QString filename) {
 	m_roiFile = filename.toStdString();
-	m_settings.setValue("lastROI", filename);
+	m_settings.setValue(LAST_ROI, filename);
 	checkRun();
 }
 
-void ContremForm::txtWLChanged(QString filename) {
-	m_wlMapFile = filename.toStdString();
-	m_settings.setValue("lastWL", filename);
+void ContremForm::cboROITypeChanged(QString value) {
+	m_roiType = value.toStdString();
 	checkRun();
 }
 
-void ContremForm::txtSpectraChanged(QString filename) {
+void ContremForm::updateSpectraType() {
+	std::string fileType = fileTypeAsString(getFileType(m_spectraFile));
+	cboSpectraType->setCurrentText(fileType.c_str());
+}
+
+void ContremForm::txtSpectraFileChanged(QString filename) {
 	m_spectraFile = filename.toStdString();
-	m_settings.setValue("lastSpectra", filename);
+	updateSpectraType();
+	loadWavelengths(m_spectraFile);
+	m_settings.setValue(LAST_SPECTRA, filename);
 	checkRun();
 }
 
-void ContremForm::txtOutputChanged(QString filename) {
+void ContremForm::cboSpectraTypeChanged(QString value) {
+	m_spectraType = value.toStdString();
+	checkRun();
+}
+
+void ContremForm::updateOutputType() {
+	std::string fileType = fileTypeAsString(getFileType(m_outputFile));
+	cboOutputType->setCurrentText(fileType.c_str());
+}
+
+void ContremForm::txtOutputFileChanged(QString filename) {
 	m_outputFile = filename.toStdString();
-	m_settings.setValue("lastOutput", filename);
+	m_settings.setValue(LAST_OUTPUT, filename);
+	checkRun();
+}
+
+void ContremForm::cboOutputTypeChanged(QString value) {
+	m_outputType = value.toStdString();
 	checkRun();
 }
 
 void ContremForm::spnMinWLChanged(double value) {
 	m_minWl = value;
-	m_settings.setValue("lastMinWL", value);
+	m_settings.setValue(LAST_MIN_WL, value);
 	checkRun();
 }
 
 void ContremForm::spnMaxWLChanged(double value) {
 	m_maxWl = value;
-	m_settings.setValue("lastMaxWL", value);
-	checkRun();
-}
-
-void ContremForm::spnWLColChanged(int value) {
-	m_wlCol = value;
-	m_settings.setValue("lastWLCol", value);
-	checkRun();
-}
-
-void ContremForm::spnBandColChanged(int value) {
-	m_bandCol = value;
-	m_settings.setValue("lastBandCol", value);
+	m_settings.setValue(LAST_MAX_WL, value);
 	checkRun();
 }
 
 void ContremForm::btnROIClicked() {
-	QString lastDir = m_settings.value("lastDir", "").toString();
+	QString lastDir = m_settings.value(LAST_DIR, "").toString();
 	QString filename = QFileDialog::getOpenFileName(this, "ENVI ROI File", lastDir);
 	QFileInfo dir(filename);
-	m_settings.setValue("lastDir", dir.dir().absolutePath());
-	txtROI->setText(filename);
-}
-
-void ContremForm::btnWLClicked() {
-	QString lastDir = m_settings.value("lastDir", "").toString();
-	QString filename = QFileDialog::getOpenFileName(this, "ENVI ROI File", lastDir);
-	QFileInfo dir(filename);
-	m_settings.setValue("lastDir", dir.dir().absolutePath());
-	txtWavelength->setText(filename);
+	m_settings.setValue(LAST_DIR, dir.dir().absolutePath());
+	txtROIFile->setText(filename);
 }
 
 void ContremForm::btnSpectraClicked() {
-	QString lastDir = m_settings.value("lastDir", "").toString();
+	QString lastDir = m_settings.value(LAST_DIR, "").toString();
 	QString filename = QFileDialog::getOpenFileName(this, "Spectra File", lastDir);
 	QFileInfo dir(filename);
-	m_settings.setValue("lastDir", dir.dir().absolutePath());
-	txtSpectra->setText(filename);
+	m_settings.setValue(LAST_DIR, dir.dir().absolutePath());
+	txtSpectraFile->setText(filename);
 }
 
 void ContremForm::btnOutputClicked() {
-	QString lastDir = m_settings.value("lastDir", "").toString();
+	QString lastDir = m_settings.value(LAST_DIR, "").toString();
 	QString filename = QFileDialog::getSaveFileName(this, "Output File", lastDir);
 	QFileInfo dir(filename);
-	m_settings.setValue("lastDir", dir.dir().absolutePath());
-	txtOutput->setText(filename);
-}
-
-void _run(ContremListener* form, Contrem* contrem, Reader* reader) {
-	contrem->run(form, reader);
+	m_settings.setValue(LAST_DIR, dir.dir().absolutePath());
+	txtOutputFile->setText(filename);
 }
 
 void ContremForm::runState() {
 	btnRun->setEnabled(false);
 	btnCancel->setEnabled(true);
-	txtROI->setEnabled(false);
-	txtWavelength->setEnabled(false);
+	txtROIFile->setEnabled(false);
 	btnROI->setEnabled(false);
-	btnWavelength->setEnabled(false);
-	txtSpectra->setEnabled(false);
+	txtSpectraFile->setEnabled(false);
 	btnSpectra->setEnabled(false);
-	txtOutput->setEnabled(false);
+	txtOutputFile->setEnabled(false);
 	btnOutput->setEnabled(false);
-	spnWLCol->setEnabled(false);
-	spnBandCol->setEnabled(false);
-	chkHeader->setEnabled(false);
-	cboDriver->setEnabled(false);
-	spnMinWL->setEnabled(false);
-	spnMaxWL->setEnabled(false);
-	rdoPopulation->setEnabled(false);
-	rdoSample->setEnabled(false);
-	spnBuffer->setEnabled(false);
-	spnThreads->setEnabled(false);
+	cboOutputType->setEnabled(false);
 }
 
 void ContremForm::stopState() {
 	btnRun->setEnabled(true);
 	btnCancel->setEnabled(false);
-	txtROI->setEnabled(true);
-	txtWavelength->setEnabled(true);
+	txtROIFile->setEnabled(true);
 	btnROI->setEnabled(true);
-	btnWavelength->setEnabled(true);
-	txtSpectra->setEnabled(true);
+	txtSpectraFile->setEnabled(true);
 	btnSpectra->setEnabled(true);
-	txtOutput->setEnabled(true);
+	txtOutputFile->setEnabled(true);
 	btnOutput->setEnabled(true);
-	spnWLCol->setEnabled(true);
-	spnBandCol->setEnabled(true);
-	chkHeader->setEnabled(true);
-	cboDriver->setEnabled(true);
-	spnMinWL->setEnabled(true);
-	spnMaxWL->setEnabled(true);
-	rdoPopulation->setEnabled(true);
-	rdoSample->setEnabled(true);
-	spnBuffer->setEnabled(true);
-	spnThreads->setEnabled(true);
+	cboOutputType->setEnabled(true);
 }
 
 void ContremForm::run() {
@@ -271,9 +334,8 @@ void ContremForm::run() {
 		m_running = true;
 
 		m_contrem->bufferSize = m_buffer;
-		m_contrem->driver = m_outputDriver;
+		m_contrem->driver = m_outputType;
 		m_contrem->outfile = m_outputFile;
-		m_contrem->sampleStats = !m_popStats;
 		m_contrem->threads = m_threads;
 
 		if(m_reader)
@@ -286,22 +348,15 @@ void ContremForm::run() {
 			throw std::invalid_argument("No input file (-r or -d) given.");
 		}
 
-		if(!m_wlMapFile.empty()) {
-			if(m_wlCol == -1 || m_bandCol == -1 || m_wlCol == m_bandCol)
-				throw std::invalid_argument("If a band file is given, the indices for "
-						"wavelength and band must be >=0 and different from each other.");
-			BandMapReader br(m_wlMapFile, m_wlCol, m_bandCol, m_header);
-			m_reader->setBandMap(br.bandMap());
-		}
-
 		if(m_minWl > 0 && m_maxWl > 0)
 			m_reader->setBandRange(m_minWl, m_maxWl);
 
 		m_reader->setBufSize(m_buffer);
 
-		m_thread = std::thread(_run, this, m_contrem, m_reader);
+		m_thread = std::thread(trun, this, m_contrem, m_reader);
 	}
 	if(!m_thread.joinable()) {
+		m_running = false;
 		delete m_reader;
 		m_reader = nullptr;
 		stopState();
