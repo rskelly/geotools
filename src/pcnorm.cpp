6fe94a39fb8883418b8beb4944a497a76b9d7959
rob <rob@dijital.ca>
2020-02-05 18:55:24 -0800

    pcnorm algorithm fix.

--- src/pcnorm.cpp
+++ src/pcnorm.cpp
@@ -8,28 +8,31 @@
 #include <limits>
 #include <fstream>
 
-#include <liblas/liblas.hpp>
 
-void getBounds(liblas::Reader& rdr, double* bounds) {
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Projection_traits_xy_3.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+#include <fstream>
 
-	double x, y, z;
+#include "pointcloud_reader.hpp"
 
-	rdr.Reset();
-	while(rdr.ReadNextPoint()) {
-		const liblas::Point& pt = rdr.GetPoint();
-		x = pt.GetX();
-		y = pt.GetY();
-		z = pt.GetZ();
-		if(x < bounds[0]) bounds[0] = x;
-		if(x > bounds[1]) bounds[1] = x;
-		if(y < bounds[2]) bounds[2] = y;
-		if(y > bounds[3]) bounds[3] = y;
-		if(z < bounds[4]) bounds[4] = z;
-		if(z > bounds[5]) bounds[5] = z;
-	}
-}
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Projection_traits_xy_3<K>  Gt;
+typedef CGAL::Delaunay_triangulation_2<Gt> Delaunay;
+typedef K::Point_3   Point;
+typedef Gt::Point_2  Point2;
+typedef CGAL::Delaunay_triangulation_2<Gt>::Face_handle FaceHandle;
 
-bool buildGrid(const std::vector<std::string>& infiles, double* bounds, double res, int& cols, int& rows, std::vector<float>& grid) {
+size_t buildGrid(const std::string& infile, double bounds[6], double res, int& cols, int& rows, std::vector<double>& grid) {
+
+	for(int i = 0; i < 3; ++i) bounds[i] = G_DBL_MAX_POS;
+	for(int i = 3; i < 6; ++i) bounds[i] = G_DBL_MAX_NEG;
+
+	pointcloud_reader rdr;
+
+	std::cout << "Computing bounds\n";
+	rdr.open(infile, true);
+	rdr.extendBounds(bounds);
 
 	// Increase bounds enough to add 2 cells all around.
 	bounds[0] -= res;
@@ -38,118 +41,40 @@ bool buildGrid(const std::vector<std::string>& infiles, double* bounds, double r
 	bounds[3] += res;
 
 	// Get grid size.
-	cols = (int) std::ceil((bounds[1] - bounds[0]) / res);
-	rows = (int) std::ceil((bounds[3] - bounds[2]) / res);
+	cols = (int) std::ceil((bounds[3] - bounds[0]) / res);
+	rows = (int) std::ceil((bounds[4] - bounds[1]) / res);
 
-	std::vector<float> weights(cols * rows);
+	// Accumulate the cell elevations from points.
+	grid.resize(cols * rows);
+	std::fill(grid.begin(), grid.end(), 0);
+	std::vector<int> counts(cols * rows);
+	std::fill(counts.begin(), counts.end(), 0);
+	double x, y, z;
+	int cls;
+
+	while(rdr.next(x, y, z, cls)) {
+		if(cls == 2) {
+			int c = (int) (x - bounds[0]) / res;
+			int r = (int) (y - bounds[1]) / res;
+			grid[r * cols + c] += z;
+			counts[r * cols + c]++;
+		}
+	}
+	rdr.close();
+
 	size_t count = 0;
-	{
-		// To compute weighted mean, need weights and values arrays.
-		grid.resize(cols * rows);
-		std::fill(grid.begin(), grid.end(), 0);
-		std::fill(weights.begin(), weights.end(), 0);
 
-		double px, py, pz;
-		double x, y, d, w;
-		int col, row;
-		double rad = std::pow(res, 2);
-
-		size_t num = 0;
-		for(const std::string& infile : infiles) {
-			std::cout << ++num << " of " << infiles.size() << "\n";
-			std::ifstream input(infile);
-			liblas::ReaderFactory rf;
-			liblas::Reader rdr = rf.CreateWithStream(input);
-			while(rdr.ReadNextPoint()) {
-
-				const liblas::Point& pt = rdr.GetPoint();
-				if(pt.GetClassification().GetClass() != 2)
-					continue;
-
-				px = pt.GetX();
-				py = pt.GetY();
-				pz = pt.GetZ();
-				col = (int) (px - bounds[0]) / res;
-				row = (int) (py - bounds[2]) / res;
-
-				for(int r = row - 1; r < row + 2; ++r) {
-					for(int c = col - 1; c < col + 2; ++c) {
-						if(c >= 0 && r >= 0 && c < cols && r < rows) {
-
-							x = bounds[0] + (c * res) + res * 0.5;
-							y = bounds[2] + (r * res) + res * 0.5;
-							d = std::pow(x - px, 2.0) + std::pow(y - py, 2.0);
-
-							if(d > rad)
-								continue;
-
-							w = 1.0 - d / rad;
-
-							// Accumulate the weighted heights and weights.
-							grid[r * cols + c] += pz * w;
-							weights[r * cols + c] += w;
-							++count;
-						}
-					}
-				}
-			}
+	// Compute the means.
+	for(size_t i = 0; i < grid.size(); ++i) {
+		if(counts[i]) {
+			grid[i] /= counts[i];
+			++count;
+		} else {
+			grid[i] = -9999;
 		}
 	}
 
-	if(!count) {
-		std::cout << "There are no ground points. Quitting.\n";
-		return false;
-	}
-
-	// Normalize by weights.
-	for(size_t i = 0; i < grid.size(); ++i) {
-		if(weights[i] > 0)
-			grid[i] /= weights[i];
-	}
-
-	// Fill in zeroes.
-	std::cout << "Filling gaps\n";
-	for(size_t i = 0; i < grid.size(); ++i) {
-		if(i % cols == 0)
-			std::cout << "Row " << (i / cols) << " of " << rows << "\n";
-		if(weights[i] == 0) {
-			double x, y, d, w0, rad, w = 0, s = 0;
-			int o = 2;
-			int col = i % cols;
-			int row = i / cols;
-			double cx = bounds[0] + col * res + res * 0.5;
-			double cy = bounds[2] + row * res + res * 0.5;
-			do {
-				rad = std::pow(o * res, 2);
-				for(int r = row - o; r < row + o + 1; ++r) {
-					for(int c = col - o; c < col + o + 1; ++c) {
-						if(c >= 0 && r >= 0 && c < cols && r < rows) {
-
-							if(weights[r * cols + c] == 0)
-								continue;
-
-							x = bounds[0] + (c * res) + res * 0.5;
-							y = bounds[2] + (r * res) + res * 0.5;
-							d = std::pow(x - cx, 2.0) + std::pow(y - cy, 2.0);
-
-							if(d > rad)
-								continue;
-
-							w0 = 1.0 - d / rad;
-
-							// Accumulate the weighted heights and weights.
-							s += grid[r * cols + c] * w0;
-							w += w0;
-						}
-					}
-				}
-				++o;
-			} while(w == 0);
-
-			grid[i] = s / w;
-		}
-	}
-	return true;
+	return count;
 }
 
 double bary(double x, double y,
@@ -162,58 +87,103 @@ double bary(double x, double y,
 	return (w0 * z0) + (w1 * z1) + (w2 * z2);
 }
 
-void normalize(const std::vector<std::string>& infiles, liblas::Writer& wtr,
-		double* bounds, double res, int cols, int /*rows*/, std::vector<float>& grid) {
+bool buildMesh(std::vector<double>& grid, int cols, int rows, double* bounds, double res, Delaunay& mesh) {
 
-	bounds[4] = std::numeric_limits<double>::max();
-	bounds[5] = std::numeric_limits<double>::lowest();
-
-	double z, px, py, pz, cx0, cy0, cz0, cx1, cy1, cz1, cx2, cy2, cz2, nz;
-	int col, row, col0, row0;
-
-	size_t num = 0;
-	for(const std::string& infile : infiles) {
-		std::cout << ++num << " of " << infiles.size() << "\n";
-		std::ifstream input(infile);
-		liblas::ReaderFactory rf;
-		liblas::Reader rdr = rf.CreateWithStream(input);
-		while(rdr.ReadNextPoint()) {
-			const liblas::Point& pt = rdr.GetPoint();
-			px = pt.GetX();
-			py = pt.GetY();
-			pz = pt.GetZ();
-			// Point's home cell.
-			col = (int) (px - bounds[0]) / res;
-			row = (int) (py - bounds[2]) / res;
-			// Center of home cell.
-			cx0 = bounds[0] + (col * res) + res * 0.5;
-			cy0 = bounds[2] + (row * res) + res * 0.5;
-			cz0 = grid[row * cols + col];
-			// Cell offsets.
-			col0 = px < cx0 ? col - 1 : col + 1;
-			row0 = py < cy0 ? row - 1 : row + 1;
-			// Centers of offset cells.
-			cx1 = bounds[0] + (col0 * res) + res * 0.5;
-			cy1 = bounds[2] + (row * res) + res * 0.5;
-			cz1 = grid[row * cols + col0];
-			cx2 = bounds[0] + (col * res) + res * 0.5;
-			cy2 = bounds[2] + (row0 * res) + res * 0.5;
-			cz2 = grid[row0 * cols + col];
-
-			if(cz0 == -9999.0 || cz1 == -9999.0 || cz2 == -9999.0)
-				continue;
-
-			// Get the barycentric z
-			nz = bary(px, py, cx0, cy0, cz0, cx1, cy1, cz1, cx2, cy2, cz2);
-			// Make point and write it.
-			liblas::Point pt0(pt);
-			pt0.SetZ((z = pz - nz));
-			wtr.WritePoint(pt0);
-			// Adjust bounds.
-			if(z < bounds[4]) bounds[4] = z;
-			if(z > bounds[5]) bounds[5] = z;
+	std::vector<Point> pts;
+	for(int r = 0; r < rows; ++r) {
+		for(int c = 0; c < cols; ++c) {
+			double x = bounds[0] + c * res + res * 0.5;
+			double y = bounds[1] + r * res + res * 0.5;
+			pts.emplace_back(x, y, grid[r * cols + c]);
 		}
 	}
+	mesh = Delaunay(pts.begin(), pts.end());
+	std::cout << "Delaunay: " << mesh.number_of_vertices() << "vertices.\n";
+	return true;
+}
+
+bool normalize(const std::string& infile, Delaunay& mesh, const std::string& outfile) {
+
+	pointcloud_reader rdr;
+
+	double x, y, z;
+	int cls;
+	FaceHandle faceh;
+
+	rdr.open(infile, false);
+
+	std::cout << std::setprecision(3) << std::fixed;
+	size_t n = 0;
+	while(rdr.next(x, y, z, cls)) {
+		Point2 pt(x, y, z);
+		faceh = mesh.locate(pt, faceh);
+		auto v1 = faceh->vertex(0)->point();
+		auto v2 = faceh->vertex(1)->point();
+		auto v3 = faceh->vertex(2)->point();
+		double zi = bary(x, y, v1.x(), v1.y(), v1.z(), v2.x(), v2.y(), v2.z(), v3.x(), v3.y(), v3.z());
+		rdr.setZ(zi);
+		if(++n % 1000 == 0)
+			std::cout << n << " of " << rdr.size() << " (" << ((float) n / rdr.size()) << "%)\n";
+	}
+
+	rdr.save(outfile);
+
+	return true;
+}
+
+bool normalizeRaster(const std::string& infile, std::vector<double>& grid, double* bounds,
+		double res, int cols, int rows, const std::string& outfile) {
+
+	pointcloud_reader rdr;
+
+	double x, y, z;
+	int cls;
+
+	rdr.open(infile, false);
+
+	std::cout << std::setprecision(3) << std::fixed;
+	size_t n = 0;
+	while(rdr.next(x, y, z, cls)) {
+		int col = (int) (x - bounds[0]) / res;
+		int row = (int) (y - bounds[1]) / res;
+		int col0 = col;
+		int row0 = row;
+		double cx = bounds[0] + col * res + 0.5 * res;
+		double cy = bounds[1] + row * res + 0.5 * res;
+		double z = grid[row * cols + col];
+		double z0 = -9999;
+		double z1 = -9999;
+		int ci = x < cx ? -1 : 1;
+		int cl = x < cx ? 0 : cols - 1;
+		int ri = y < cy ? -1 : 1;
+		int rl = y < cy ? 0 : rows - 1;
+		col0 += ci;
+		do {
+			if((z0 = grid[row * cols + col0]) != -9999)
+				break;
+			col0 += ci;
+		} while(col0 != cl);
+		row0 += ri;
+		do {
+			if((z1 = grid[row0 * cols + col]) != -9999)
+				break;
+			row0 += ri;
+		} while(row0 != rl);
+		if(z0 == -9999) z0 = z;
+		if(z1 == -9999) z1 = z;
+		double cx0 = bounds[0] + col0 * res + 0.5 * res;
+		double cy0 = bounds[1] + row * res + 0.5 * res;
+		double cx1 = bounds[0] + col * res + 0.5 * res;
+		double cy1 = bounds[1] + row0 * res + 0.5 * res;
+		double zi = bary(x, y, cx, cy, z, cx0, cy0, z0, cx1, cy1, z1);
+		rdr.setZ(zi);
+		if(++n % 100000 == 0)
+			std::cout << n << " of " << rdr.size() << " (" << ((float) n / rdr.size()) << "%)\n";
+	}
+
+	rdr.save(outfile);
+
+	return true;
 }
 
 int main(int argc, char** argv) {
@@ -225,49 +195,32 @@ int main(int argc, char** argv) {
 
 	std::string outfile = argv[1];
 	double resolution = atof(argv[2]);
-	std::vector<std::string> infiles;
-	for(int i = 3; i < argc; ++i)
-		infiles.push_back(argv[i]);
+	std::string infile = argv[3];
 
 	double bounds[6];
-	bounds[0] = bounds[2] = bounds[4] = std::numeric_limits<double>::max();
-	bounds[1] = bounds[3] = bounds[5] = std::numeric_limits<double>::lowest();
+	int cols, rows;
+	std::vector<double> grid;
+	Delaunay mesh;
 
-	std::unique_ptr<liblas::Header> whdr;
-
-	std::cout << "Computing bounds\n";
-	for(const std::string& infile : infiles) {
-		std::ifstream input(infile);
-		liblas::ReaderFactory rf;
-		liblas::Reader rdr = rf.CreateWithStream(input);
-		const liblas::Header& rhdr = rdr.GetHeader();
-		getBounds(rdr, bounds);
-		if(infile == infiles.front())
-			whdr.reset(new liblas::Header(rhdr));
+	std::cout << "Building grid.\n";
+	if(!buildGrid(infile, bounds, resolution, cols, rows, grid)) {
+		std::cerr << "Failed to build grid.\n";
+		return 1;
 	}
 
-	double gbounds[6];
-	for(int i = 0; i < 6; ++i)
-		gbounds[i] = bounds[i];
+	//std::cout << "Building mesh.\n";
+	//if(!buildMesh(grid, cols, rows, bounds, resolution, mesh)) {
+	//	std::cerr << "Failed to build mesh.\n";
+	//	return 1;
+	//}
+	//grid.clear();
 
-	int cols, rows;
-	std::vector<float> grid;
-
-	std::cout << "Building grid\n";
-	if(!buildGrid(infiles, gbounds, resolution, cols, rows, grid))
+	std::cout << "Normalizing.\n";
+	//if(!normalize(infile, mesh, outfile)) {
+	if(!normalizeRaster(infile, grid, bounds, resolution, cols, rows, outfile)) {
+		std::cerr << "Failed to normalize.\n";
 		return 1;
+	}
 
-	std::ofstream output;
-	liblas::WriterFactory wf;
-	liblas::Create(output, outfile);
-	liblas::Writer wtr(output, *whdr);
-
-	std::cout << "Normalizing\n";
-	normalize(infiles, wtr, gbounds, resolution, cols, rows, grid);
-
-	whdr->SetMin(bounds[0], bounds[2], gbounds[4]);
-	whdr->SetMax(bounds[1], bounds[3], gbounds[5]);
-
-	wtr.SetHeader(*whdr);
 	return 0;
 }
