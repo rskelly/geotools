3e3d165d28cb2a45f4279c1650cf502e3bad34b2
rob <rob@dijital.ca>
2020-02-06 17:30:48 -0800

    pcnorm

--- src/pcnorm.cpp
+++ src/pcnorm.cpp
@@ -8,22 +8,9 @@
 #include <limits>
 #include <fstream>
 
-
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Projection_traits_xy_3.h>
-#include <CGAL/Delaunay_triangulation_2.h>
-#include <fstream>
-
 #include "pointcloud_reader.hpp"
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Projection_traits_xy_3<K>  Gt;
-typedef CGAL::Delaunay_triangulation_2<Gt> Delaunay;
-typedef K::Point_3   Point;
-typedef Gt::Point_2  Point2;
-typedef CGAL::Delaunay_triangulation_2<Gt>::Face_handle FaceHandle;
-
-size_t buildGrid(const std::string& infile, double bounds[6], double res, int& cols, int& rows, std::vector<double>& grid) {
+size_t buildGrid(const std::string& infile, double* bounds, double res, int& cols, int& rows, std::vector<double>& grid) {
 
 	for(int i = 0; i < 3; ++i) bounds[i] = G_DBL_MAX_POS;
 	for(int i = 3; i < 6; ++i) bounds[i] = G_DBL_MAX_NEG;
@@ -33,12 +20,6 @@ size_t buildGrid(const std::string& infile, double bounds[6], double res, int& c
 	std::cout << "Computing bounds\n";
 	rdr.open(infile, true);
 	rdr.extendBounds(bounds);
-
-	// Increase bounds enough to add 2 cells all around.
-	bounds[0] -= res;
-	bounds[1] += res;
-	bounds[2] -= res;
-	bounds[3] += res;
 
 	// Get grid size.
 	cols = (int) std::ceil((bounds[3] - bounds[0]) / res);
@@ -56,6 +37,8 @@ size_t buildGrid(const std::string& infile, double bounds[6], double res, int& c
 		if(cls == 2) {
 			int c = (int) (x - bounds[0]) / res;
 			int r = (int) (y - bounds[1]) / res;
+			if(c < 0 || r < 0 || c >= cols || r >= rows)
+				continue; //throw std::runtime_error("out of range");
 			grid[r * cols + c] += z;
 			counts[r * cols + c]++;
 		}
@@ -74,6 +57,34 @@ size_t buildGrid(const std::string& infile, double bounds[6], double res, int& c
 		}
 	}
 
+	size_t filled;
+	double s, v;
+	int ct;
+	do {
+		filled = 0;
+		for(int r = 0; r < rows; ++r) {
+			for(int c = 0; c < cols; ++c) {
+				s = 0;
+				ct = 0;
+				if(grid[r * cols + c] == -9999) {
+					for(int rr = r - 1; rr < r + 2; ++rr) {
+						for(int cc = c - 1; cc < c + 2; ++cc) {
+							if(!(rr < 0 || cc < 0 || (rr == r && cc == c) || rr >= rows || cc >= cols)
+									&& (v = grid[rr * cols + cc]) != -9999) {
+								s += v;
+								++ct;
+							}
+						}
+					}
+				}
+				if(ct) {
+					grid[r * cols + c] = s / ct;
+					++filled;
+				}
+			}
+		}
+	} while(filled);
+
 	return count;
 }
 
@@ -87,50 +98,6 @@ double bary(double x, double y,
 	return (w0 * z0) + (w1 * z1) + (w2 * z2);
 }
 
-bool buildMesh(std::vector<double>& grid, int cols, int rows, double* bounds, double res, Delaunay& mesh) {
-
-	std::vector<Point> pts;
-	for(int r = 0; r < rows; ++r) {
-		for(int c = 0; c < cols; ++c) {
-			double x = bounds[0] + c * res + res * 0.5;
-			double y = bounds[1] + r * res + res * 0.5;
-			pts.emplace_back(x, y, grid[r * cols + c]);
-		}
-	}
-	mesh = Delaunay(pts.begin(), pts.end());
-	std::cout << "Delaunay: " << mesh.number_of_vertices() << "vertices.\n";
-	return true;
-}
-
-bool normalize(const std::string& infile, Delaunay& mesh, const std::string& outfile) {
-
-	pointcloud_reader rdr;
-
-	double x, y, z;
-	int cls;
-	FaceHandle faceh;
-
-	rdr.open(infile, false);
-
-	std::cout << std::setprecision(3) << std::fixed;
-	size_t n = 0;
-	while(rdr.next(x, y, z, cls)) {
-		Point2 pt(x, y, z);
-		faceh = mesh.locate(pt, faceh);
-		auto v1 = faceh->vertex(0)->point();
-		auto v2 = faceh->vertex(1)->point();
-		auto v3 = faceh->vertex(2)->point();
-		double zi = bary(x, y, v1.x(), v1.y(), v1.z(), v2.x(), v2.y(), v2.z(), v3.x(), v3.y(), v3.z());
-		rdr.setZ(zi);
-		if(++n % 1000 == 0)
-			std::cout << n << " of " << rdr.size() << " (" << ((float) n / rdr.size()) << "%)\n";
-	}
-
-	rdr.save(outfile);
-
-	return true;
-}
-
 bool normalizeRaster(const std::string& infile, std::vector<double>& grid, double* bounds,
 		double res, int cols, int rows, const std::string& outfile) {
 
@@ -141,44 +108,44 @@ bool normalizeRaster(const std::string& infile, std::vector<double>& grid, doubl
 
 	rdr.open(infile, false);
 
-	std::cout << std::setprecision(3) << std::fixed;
 	size_t n = 0;
-	while(rdr.next(x, y, z, cls)) {
+	size_t idx;
+	while(rdr.next(x, y, z, cls, idx)) {
 		int col = (int) (x - bounds[0]) / res;
 		int row = (int) (y - bounds[1]) / res;
 		int col0 = col;
 		int row0 = row;
-		double cx = bounds[0] + col * res + 0.5 * res;
-		double cy = bounds[1] + row * res + 0.5 * res;
-		double z = grid[row * cols + col];
-		double z0 = -9999;
+		double cx0 = bounds[0] + col * res + 0.5 * res;
+		double cy0 = bounds[1] + row * res + 0.5 * res;
+		double z0 = grid[row * cols + col];
 		double z1 = -9999;
-		int ci = x < cx ? -1 : 1;
-		int cl = x < cx ? 0 : cols - 1;
-		int ri = y < cy ? -1 : 1;
-		int rl = y < cy ? 0 : rows - 1;
+		double z2 = -9999;
+		int ci = x < cx0 ? -1 : 1;
+		int cl = x < cx0 ? 0 : cols - 1;
+		int ri = y < cy0 ? -1 : 1;
+		int rl = y < cy0 ? 0 : rows - 1;
 		col0 += ci;
 		do {
-			if((z0 = grid[row * cols + col0]) != -9999)
+			if(col0 < 0 || col0 >= cols || (z1 = grid[row * cols + col0]) != -9999)
 				break;
 			col0 += ci;
 		} while(col0 != cl);
 		row0 += ri;
 		do {
-			if((z1 = grid[row0 * cols + col]) != -9999)
+			if(row0 < 0 || row0 >= rows || (z2 = grid[row0 * cols + col]) != -9999)
 				break;
 			row0 += ri;
 		} while(row0 != rl);
-		if(z0 == -9999) z0 = z;
-		if(z1 == -9999) z1 = z;
-		double cx0 = bounds[0] + col0 * res + 0.5 * res;
-		double cy0 = bounds[1] + row * res + 0.5 * res;
-		double cx1 = bounds[0] + col * res + 0.5 * res;
-		double cy1 = bounds[1] + row0 * res + 0.5 * res;
-		double zi = bary(x, y, cx, cy, z, cx0, cy0, z0, cx1, cy1, z1);
-		rdr.setZ(zi);
-		if(++n % 100000 == 0)
-			std::cout << n << " of " << rdr.size() << " (" << ((float) n / rdr.size()) << "%)\n";
+		if(z1 == -9999) z1 = z0;
+		if(z2 == -9999) z2 = z0;
+		double cx1 = bounds[0] + col0 * res + 0.5 * res;
+		double cy1 = bounds[1] + row * res + 0.5 * res;
+		double cx2 = bounds[0] + col * res + 0.5 * res;
+		double cy2 = bounds[1] + row0 * res + 0.5 * res;
+		double zi = bary(x, y, cx0, cy0, z0, cx1, cy1, z1, cx2, cy2, z2);
+		rdr.setZ(idx, z - zi);
+		if(++n % 1000000 == 0)
+			std::cout << (n * 100) / rdr.size() << "%\n";
 	}
 
 	rdr.save(outfile);
@@ -200,7 +167,6 @@ int main(int argc, char** argv) {
 	double bounds[6];
 	int cols, rows;
 	std::vector<double> grid;
-	Delaunay mesh;
 
 	std::cout << "Building grid.\n";
 	if(!buildGrid(infile, bounds, resolution, cols, rows, grid)) {
@@ -208,15 +174,7 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	//std::cout << "Building mesh.\n";
-	//if(!buildMesh(grid, cols, rows, bounds, resolution, mesh)) {
-	//	std::cerr << "Failed to build mesh.\n";
-	//	return 1;
-	//}
-	//grid.clear();
-
 	std::cout << "Normalizing.\n";
-	//if(!normalize(infile, mesh, outfile)) {
 	if(!normalizeRaster(infile, grid, bounds, resolution, cols, rows, outfile)) {
 		std::cerr << "Failed to normalize.\n";
 		return 1;
