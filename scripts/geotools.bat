bfa6b3b5d54999a6f88cb6086c8c050d0911101b
Rob <rob@dijital.ca>
2020-09-17 18:55:42 -0700

    Small installer for windows.

---
+++ scripts/geotools.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rem Start script for windows install.
+
+set PATH=%PATH%;%cd%
+
+echo Welcome to geotools!
+echo Type pc2grid, voidfill or pcnorm to see instructions.
+echo (voidfill doesn't work perfectly so be warned.)
+echo You can cd from here to any directory to do your work
+echo but don't close the cmd window or the environment will
+echo expire. Open the cmd window from the dijital.ca menu to
+echo reopen it.
+
+cmd /k
+
