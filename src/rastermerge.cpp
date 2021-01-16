6d482bb1f061a244ca44ff82b334a0422de8ba18
Rob <rob@dijital.ca>
2021-01-16 11:08:26 -0800

    Fixed rastermerge.
    Performs multiple passes with separate resample and radius values.
    Performs two steps: 1) distance weighted averaging of pixels; 2)
    distance weighted decay to zero relative to nearest valid pixel in the
    diff raster.

--- src/rastermerge.cpp
+++ src/rastermerge.cpp
@@ -96,261 +96,130 @@ bool smooth(std::vector<bool>& filled, Band<float>& src, Band<float>& dst, int c
 	return false;
 }
 
-void processIDW(std::list<std::pair<int, int>>* rowq, std::mutex* qmtx, Band<float>* src, Band<float>* dst, std::mutex* dmtx, int size) {
+/**
+ * Process inverse distance waiting.
+ * \param rowq A queue of row index/block height pairs.
+ * \param qmtx A mutex to protect the queue.
+ * \param src The source (difference) raster.
+ * \param dst The target (output) raster.
+ * \param dmtx A mutex to protect the output raster.
+ * \param size The search radius in pixels.
+ */
+void processIDW(std::list<std::pair<int, int>>* rowq, std::mutex* qmtx,
+		Band<float>* src, Band<float>* dst, std::mutex* dmtx, int size) {
+
+	// The row index and block height.
 	int row, block;
+
+	// The source and destination properties.
 	const GridProps& sprops = src->props();
 	const GridProps& dprops = dst->props();
 	
 	// Row data buffer.
 	std::vector<float> rowbuf;
 
+	// Cols and rows in the destination raster.
 	int dcols = dst->props().cols();
 	int drows = dst->props().rows();
 
 	while(!rowq->empty()) {
 		{
+			// Grab a row/block pair off the queue.
 			std::lock_guard<std::mutex> lk(*qmtx);
 			if(!rowq->empty()) {
 				std::pair<int, int>& item = rowq->front();
 				row = item.first;
 				block = item.second;
 				rowq->pop_front();
-				std::cout << "Row " << row << " of " << sprops.rows() << "\n";
+				std::cout << "Row " << row << " of " << drows << "\n";
 			} else {
 				std::this_thread::yield();
 				continue;
 			}
 		}
 
+		// If the block size is too large, shrink it.
 		if(row + block >= drows)
 			block = drows - row;
 
-		// Row data buffer.
+		// Resize and fill the row data buffer.
 		rowbuf.resize(dcols * block);
 		std::fill(rowbuf.begin(), rowbuf.end(), dprops.nodata());
 
+		// Squared radius for validation.
 		float maxrad = size * size;
+		float nearest;
+		// Raster values.
 		float v0, v1;
 		for(int b = 0; b < block; ++b) {
 			for(int col = 0; col < dcols; ++col) {
+				// If the destination pixel is invalid skip it.
 				if((v1 = dst->get(col, row + b)) == dprops.nodata())
 					continue;
+				// The sum and weight variables.
 				float s = 0;
 				float w = 0;
+				// If true, exit the loops.
 				bool halt = false;
+				// Col and row in the difference raster.
 				int scol = sprops.toCol(dprops.toX(col));
 				int srow = sprops.toRow(dprops.toY(row));
+				nearest = geo::maxvalue<float>();
+				// Run the circular kernel.
 				for(int r = -size; !halt && r < size+ 1; ++r) {
 					for(int c = -size; c < size + 1; ++c) {
+						// Calculate the kernel pixel.
 						int cc = scol + c;
 						int rr = srow + r;
+						// The distance from the kernel center.
 						float d = (float) (c * c + r * r);
-						if(d <= maxrad && sprops.hasCell(cc, rr) && (v0 = src->get(cc, rr)) != sprops.nodata()) {
-							if(d == 0) {
+						// If the distance is within the max radius, and the pixel is valid, add to sum and weight.
+						if(d <= maxrad
+								&& sprops.hasCell(cc, rr)
+								&& (v0 = src->get(cc, rr)) != sprops.nodata()) {
+							if(false) { //d == 0) {
+								// If the distance is zero, use the value directly and halt.
 								s = v0;
 								w = 1;
 								halt = true;
 								break;
 							} else {
-								float w0 = 1.0f / d;
+								// Add to sum and weight.
+								if(d < nearest)
+									nearest = d;
+								float w0 = 1.0f - (d / maxrad);
 								s += v0 * w0;
 								w += w0;
 							}
 						}
 					}
 				}
-				if(w > 0)
-					rowbuf[b * dcols + col] = v1 + s / w;
+				// If the weight is valid, adjust the value.
+				if(w > 0) {
+					rowbuf[b * dcols + col] = v1 + (s / w) * (1.0f - nearest / maxrad);
+				} else {
+					rowbuf[b * dcols + col] = v1;
+				}
 			}
 		}
+
+		// Write the block to the output raster.
 		std::lock_guard<std::mutex> lk(*dmtx);
 		for(int b = 0; b < block; ++b)
-			dst->setRow(row + b, rowbuf.data() + b * dcols);
+			dst->setRow(row + b, rowbuf.data() + (b * dcols));
 	}
 }
-
-void processDW(std::list<std::pair<int, int>>* rowq, std::mutex* qmtx, Band<float>* src, Band<float>* dst, std::mutex* dmtx, int size) {
-	int row, block;
-	int half = size / 2 + 1; // Split the size in half for the radius of the kernel.
-	const GridProps& sprops = src->props();
-	const GridProps& dprops = dst->props();
-
-	// Row data buffer.
-	std::vector<float> rowbuf;
-
-	int dcols = dst->props().cols();
-	int drows = dst->props().rows();
-
-	while(!rowq->empty()) {
-		{
-			std::lock_guard<std::mutex> lk(*qmtx);
-			if(!rowq->empty()) {
-				std::pair<int, int>& item = rowq->front();
-				row = item.first;
-				block = item.second;
-				rowq->pop_front();
-				std::cout << "Row " << row << " of " << sprops.rows() << "\n";
-			} else {
-				std::this_thread::yield();
-				continue;
-			}
-		}
-
-		if(row + block >= drows)
-			block = drows - row;
-
-		rowbuf.resize(dcols * block);
-		std::fill(rowbuf.begin(), rowbuf.end(), dprops.nodata());
-
-		float v0, v1;
-		for(int b = 0; b < block; ++b) {
-			for(int col = 0; col < dcols; ++col) {
-				if((v1 = dst->get(col, row + b)) == dprops.nodata())
-					continue;
-				float s = 0;
-				float w = 0;
-				int scol = sprops.toCol(dprops.toX(col));
-				int srow = sprops.toRow(dprops.toY(row));
-				for(int r = -half; r < half + 1; ++r) {
-					for(int c = -half; c < half + 1; ++c) {
-						int cc = scol + c;
-						int rr = srow + r;
-						float d = 1.0f - std::sqrt((float) (c * c + r * r)) / half;
-						if(d <= 1.0f && sprops.hasCell(cc, rr) && (v0 = src->get(cc, rr)) != sprops.nodata()) {
-							s += v0 * d;
-							w += d;
-						}
-					}
-				}
-				if(w > 0 && (v1 = dst->get(col, row)) != dprops.nodata())
-					rowbuf[b * drows + col] = v1 + s / w;
-			}
-		}
-		std::lock_guard<std::mutex> lk(*dmtx);
-		for(int b = 0; b < block; ++b)
-			dst->setRow(row + b, rowbuf.data() + b * dcols);
-	}
-}
-
-void processCos(std::list<std::pair<int, int>>* rowq, std::mutex* qmtx, Band<float>* src, Band<float>* dst, std::mutex* dmtx, int size, float* cos) {
-	float rad2 = std::pow(size / 2.0, 2.0) / 1000;	// Because the cosine lookup has 1000 elements.
-	int row, block;
-	const GridProps& sprops = src->props();
-	while(!rowq->empty()) {
-		{
-			std::lock_guard<std::mutex> lk(*qmtx);
-			if(!rowq->empty()) {
-				std::pair<int, int>& item = rowq->front();
-				row = item.first;
-				block = item.second;
-				rowq->pop_front();
-				std::cout << "Row " << row << " of " << sprops.rows() << "\n";
-			} else {
-				std::this_thread::sleep_for(std::chrono::milliseconds(1));
-				continue;
-			}
-		}
-
-		float v0;
-		for(int col = 0; col < sprops.cols(); ++col) {
-			float s = 0;
-			float w = 0;
-			bool halt = false;
-			for(int r = -size / 2; !halt && r < size / 2 + 1; ++r) {
-				for(int c = -size / 2; c < size / 2 + 1; ++c) {
-					int cc = col + c;
-					int rr = row + r;
-					if(cc < 0 || rr < 0 || cc >= sprops.cols() || rr >= sprops.rows())
-						continue;
-					if((v0 = src->get(cc, rr)) != sprops.nodata()) {
-						int d = (int) std::min(1000.0f, (float) (c * c + r * r) / rad2);
-						//std::cout << d << " " << c << " " << r << " " << (c * c + r * r) << " " << rad2 << "\n";
-						if(d < 1000){
-							float w0 = cos[d];
-							s += v0 * w0;
-							w += 1;
-						}
-					}
-				}
-			}
-			if(w > 0) {
-				std::lock_guard<std::mutex> lk(*dmtx);
-				dst->set(col, row, s / w);
-			}
-		}
-	}
-}
-
-
-void processGauss(std::list<std::pair<int, int>>* rowq, std::mutex* qmtx, Band<float>* src, Band<float>* dst, std::mutex* dmtx, int size, float sigma) {
-	int row, block;
-	const GridProps& sprops = src->props();
-	while(!rowq->empty()) {
-		{
-			std::lock_guard<std::mutex> lk(*qmtx);
-			if(!rowq->empty()) {
-				std::pair<int, int>& item = rowq->front();
-				row = item.first;
-				block = item.second;
-				rowq->pop_front();
-				std::cout << "Row " << row << " of " << sprops.rows() << "\n";
-			} else {
-				std::this_thread::sleep_for(std::chrono::milliseconds(1));
-				continue;
-			}
-		}
-
-		float gain = 0.5;
-		float v0;
-		for(int col = 0; col < sprops.cols(); ++col) {
-			float s = 0;
-			float w = 0;
-			bool halt = false;
-			for(int r = -size / 2; !halt && r < size / 2 + 1; ++r) {
-				for(int c = -size / 2; c < size / 2 + 1; ++c) {
-					int cc = col + c;
-					int rr = row + r;
-					if(cc < 0 || rr < 0 || cc >= sprops.cols() || rr >= sprops.rows())
-						continue;
-					if((v0 = src->get(cc, rr)) != sprops.nodata()) {
-						float w0 = std::exp(-0.5 * (c * c + r * r) / (sigma * sigma));
-						//std::cout << d << " " << c << " " << r << " " << (c * c + r * r) << " " << rad2 << "\n";
-						s += v0 * w0;
-						w += gain;
-					}
-				}
-			}
-			if(w > 0) {
-				std::lock_guard<std::mutex> lk(*dmtx);
-				dst->set(col, row, s / w);
-			}
-		}
-	}
-}
-
-class Pt {
-public:
-	double x, y, z;
-	Pt(double x, double y, double z) : x(x), y(y), z(z) {}
-	double& operator[](int idx) {
-		switch(idx % 3) {
-		case 0: return x;
-		case 1: return y;
-		case 2:
-		default: return z;
-		}
-	}
-};
 
 int main(int argc, char** argv) {
 
 	if(argc < 6) {
 		std::cerr << "Usage: rastermerge [options] <<anchor file 1> <anchor band 1> [<anchor file 2> <anchor band 2> [...]]> <target file 2> <target band 2> <output file>\n"
-				<< " -s <size>          The radius of the window in map units.\n"
-				<< " -t <threads>       The number of threads.\n"
-				<< " -m <method>        The method: idw, dw, gauss, cosine. Default IDW.\n"
-				<< " -k <mask> <band>   A mask file. Pixel value 1 is kept.\n"
+				<< " -s <size>          The radius of the window in map units. If a comma-delimited list, will make\n"
+				<< "                    multiple passes.\n"
 				<< " -r <mult>          A resample multiplier. 1 is no change, 2 doubles the side of a pixel, etc.\n"
+				<< "                    If -s is a list, this must be a list of the same length.\n"
+				<< " -t <threads>       The number of threads.\n"
+				<< " -k <mask> <band>   A mask file. Pixel value 1 is kept.\n"
 				<< " -y                 If given, use in-core memory instead of mapped.\n"
 				<< " -b <block size>    The height of processed rows. Blocks are processed in parallel. Default 1\n";
 		return 1;
@@ -358,26 +227,31 @@ int main(int argc, char** argv) {
 
 	std::vector<std::string> files;
 	std::vector<int> bands;
-	float radius = 100;
+	std::vector<float> radii;
+	std::vector<int> resamples;
 	int tcount = 4;
-	std::string method = "idw";
 	int block = 1;
 	std::string outfile;
 	std::string maskfile;
 	int maskband = 0;
 	bool mapped = true;
-	int resample = 1;
 
 	for(int i = 1; i < argc; ++i) {
 		std::string arg(argv[i]);
 		if(arg == "-s") {
-			radius = atof(argv[++i]);
+			std::vector<std::string> rad;
+			geo::util::split(std::back_inserter(rad), argv[++i], ",");
+			for(const std::string& r : rad)
+				radii.push_back(atof(r.c_str()));
+		} else if(arg == "-r") {
+			std::vector<std::string> res;
+			geo::util::split(std::back_inserter(res), argv[++i], ",");
+			for(const std::string& r : res)
+				resamples.push_back(atoi(r.c_str()));
 		} else if(arg == "-y") {
 			mapped = false;
 		} else if(arg == "-b") {
 			block = atoi(argv[++i]);
-		} else if(arg == "-r") {
-			resample = atoi(argv[++i]);
 		} else if(arg == "-k") {
 			maskfile = argv[++i];
 			maskband = atoi(argv[++i]);
@@ -385,8 +259,6 @@ int main(int argc, char** argv) {
 			tcount = atoi(argv[++i]);
 			if(tcount < 1)
 				tcount = 1;
-		} else if(arg == "-m") {
-			method = argv[++i];
 		} else {
 			if(i < argc - 1) {
 				files.push_back(arg);
@@ -397,6 +269,11 @@ int main(int argc, char** argv) {
 		}
 	}
 
+	if(radii.empty())
+		g_runerr("No radii specified. Need at least one.");
+	if(radii.size() != resamples.size())
+		g_runerr("The radius and resample lists should be the same length.");
+
 	g_debug("Using mapped memory: " << mapped);
 
 	std::string targetFile = files.back();
@@ -405,113 +282,136 @@ int main(int argc, char** argv) {
 	std::string filebase = outfile.substr(0, outfile.find('.'));
 	std::cout << filebase << "\n";
 
-	Band<float> rdiff;
-	Band<float> target;
-	GridProps tprops;
+	Band<float> output;		// The output raster. Target is loaded, then modified and written.
+	GridProps oprops;		// The output raster properties.
+
+	// Load the target/output raster.
+	g_debug("Loading target raster.");
 	{
-		Band<float> anchor;
-		GridProps aprops;
-		GridProps mprops;
-		
-		target.init(targetFile, targetBand - 1, true, mapped);
-		tprops = target.props();
+		Band<float> target(targetFile, targetBand - 1, false, mapped);
+		oprops = target.props();
+		oprops.setWritable(true);
+		oprops.setBands(1);
+		output.init(outfile, oprops, mapped);
+		target.writeTo(output);
+	}
 
-		GridProps dprops(target.props());
-		dprops.setNoData(-9999);
+	// Load the anchor rasters(s).
+	g_debug("Loading anchor raster.")
+	Band<float> anchor;	// The anchor raster.
+	loadRasters("/tmp/anchor.tif", files, bands, files.size() - 1, 1, anchor, mapped);
+	GridProps aprops = anchor.props();
 
-		GridProps rprops(dprops);
-		if(resample > 1)
+	// Load the mask raster if available.
+	bool hasMask = !maskfile.empty();
+	Band<int> mask;		// The mask raster.
+	GridProps mprops;	// The mask raster properties.
+	if(hasMask) {
+		g_debug("Loading mask raster.")
+		mask.init(maskfile, maskband - 1, false, mapped);
+		mprops = mask.props();
+	}
+
+	for(int i = 0; i < (int)radii.size(); ++i) {
+		float radius = radii[i];
+		int resample = resamples[i];
+
+		g_debug("Pass " << (i + 1) << ": radius: " << radius << "; resample: " << resample);
+
+		// Initialized the (scaled) difference raster.
+		Band<float> rdiff;
+		GridProps rprops(oprops);
+
+		if(resample > 1) {
+			rprops.setResolution(rprops.resX() * resample, rprops.resY() * resample);
 			rprops.setSize(rprops.cols() / resample + 1, rprops.rows() / resample + 1);
-		rdiff.init("/tmp/rdiff.tif", rprops, mapped);
-		rdiff.fill(0);
-
-		loadRasters("/tmp/anchor.tif", files, bands, files.size() - 1, 1, anchor, mapped);
-		aprops = anchor.props();
-
-		bool hasMask = !maskfile.empty();
-		Band<int> mask;
-		if(hasMask) {
-			mask.init(maskfile, maskband - 1, false, mapped);
-			mprops = mask.props();
 		}
+		rdiff.init("/tmp/rdiff.tif", rprops, mapped);
+		rdiff.fill(rprops.nodata());
 
-		for(int row1 = 0; row1 < tprops.rows(); ++row1) {
+		// Calculate the difference between anchor and target rasters.
+		g_debug("Calculating difference raster.");
+		int rows = oprops.rows();
+		int cols = oprops.cols();
+		float nd = oprops.nodata();
+		float v1, v2, v;
+		for(int row1 = 0; row1 < rows; ++row1) {
 			if(row1 % 100)
-				g_debug("Row " << row1 << " of " << tprops.rows());
-			float v1, v2;
-			for(int col1 = 0; col1 < tprops.cols(); ++col1) {
+				g_debug("Row " << row1 << " of " << rows);
+			for(int col1 = 0; col1 < cols; ++col1) {
 
-				if((v1 = target.get(col1, row1)) == tprops.nodata() || std::isnan(v1))
+				// If the target raster is invalid, skip.
+				if((v1 = output.get(col1, row1)) == nd || std::isnan(v1))
 					continue;
 
-				double x = tprops.toX(col1);
-				double y = tprops.toY(row1);
+				// Convert the target cell coords to the anchor and mask cell coords.
+				double x = oprops.toX(col1);
+				double y = oprops.toY(row1);
 				int col2 = aprops.toCol(x);
 				int row2 = aprops.toRow(y);
 				int mc = mprops.toCol(x);
 				int mr = mprops.toRow(y);
-						
+
+				// If the anchor raster has the cell, the mask is valid and the anchor value is valid,
+				// don't skip.
 				if(!aprops.hasCell(col2, row2)
-//						&& (!hasMask || (mprops.hasCell(mc, mr) && mask.get(mc, mr) == 1))
-						|| (v2 = anchor.get(col2, row2)) == aprops.nodata() || std::isnan(v2))
+					|| (v2 = anchor.get(col2, row2)) == aprops.nodata()
+					|| std::isnan(v2)
+					|| (hasMask && !(mprops.hasCell(mc, mr) && mask.get(mc, mr) == 1)))
 					continue;
 						
-				rdiff.set(col1 / resample, row1 / resample, rdiff.get(col1 / resample, row1 / resample) + v2 - v1);
+				// Add the difference to the difference raster. If the current value
+				// is nodata, set it to zero.
+				if((v = rdiff.get(col1 / resample, row1 / resample)) == rprops.nodata())
+					v = 0;
+				rdiff.set(col1 / resample, row1 / resample, v + v2 - v1);
 			}
 		}
 
-		for(int row = 0; row < rprops.rows(); ++row) {
-			for(int col = 0; col < rprops.cols(); ++col) {
-				rdiff.set(col, row, rdiff.get(col, row) / (resample * resample));
+		// Recalculate the average by the resample factor if it's larger than 1.
+		if(resample > 1) {
+			g_debug("Normalizing difference raster.")
+			rows = rprops.rows();
+			cols = rprops.cols();
+			for(int row = 0; row < rows; ++row) {
+				for(int col = 0; col < cols; ++col) {
+					if((v = rdiff.get(col, row)) != rprops.nodata())
+						rdiff.set(col, row, v / (resample * resample));
+				}
 			}
 		}
-	}
 
-	{
+		{
+			// Queue contains start index of row and number of rows.
+			g_debug("Building row queue.");
+			std::list<std::pair<int, int>> rowq;
+			for(int row = 0; row < oprops.rows(); row += block)
+				rowq.emplace_back(row, block);
 
-		Band<float> output(outfile, tprops, mapped);
-		target.writeTo(output);	
-		
-		// Queue contains start index of row and number of rows.
-		std::list<std::pair<int, int>> rowq;
-		for(int row = 0; row < rdiff.props().rows(); row += block)
-			rowq.emplace_back(row, block);
+			std::mutex qmtx;
+			std::mutex dmtx;
+			std::vector<std::thread> threads;
 
-		std::mutex qmtx;
-		std::mutex dmtx;
-		std::vector<std::thread> threads;
+			// Calculate the search radius in columns.
+			int size = std::ceil((radius * 2) / std::abs(rdiff.props().resX()));
+			// If the resample value is >1, recalculate the cell radius.
+			if(resample > 1)
+				size = size / resample + 1;
+			// The cell radius has to be odd.
+			if(size % 2 == 0)
+				size++;
 
-		// Calculate the search radius in columns.
-		int size = std::ceil((radius * 2) / std::abs(rdiff.props().resX()));
-		if(resample > 1)
-			size = size / resample + 1;
-		if(size % 2 == 0)
-			size++;
-
-		if(method == "gauss") {
-			for(int i = 0; i < tcount; ++i)
-				threads.emplace_back(processGauss, &rowq, &qmtx, &rdiff, &output, &dmtx, size, (float) size * 0.25);
-		} else if(method == "cosine") {
-			float cos[1001];
-			for(int i = 0; i <= 1000; ++i)
-				cos[i] = std::cos((float) i / 1000 * M_PI) / 2.0 + 0.5;
-			for(int i = 0; i < tcount; ++i)
-				threads.emplace_back(processCos, &rowq, &qmtx, &rdiff, &output, &dmtx, size, cos);
-		} else if(method == "idw") {
+			g_debug("Starting processor threads.");
 			for(int i = 0; i < tcount; ++i)
 				threads.emplace_back(processIDW, &rowq, &qmtx, &rdiff, &output, &dmtx, size);
-		} else if(method == "dw") {
-			for(int i = 0; i < tcount; ++i)
-				threads.emplace_back(processDW, &rowq, &qmtx, &rdiff, &output, &dmtx, size);
-		} else {
-			std::cerr << "Unknown method: " << method << "\n";
-			return 1;
+
+			// Wait for threads to complete.
+			for(int i = 0; i < tcount; ++i) {
+				if(threads[i].joinable())
+					threads[i].join();
+			}
 		}
 
-		for(int i = 0; i < tcount; ++i) {
-			if(threads[i].joinable())
-				threads[i].join();
-		}
 	}
 
 	return 0;
