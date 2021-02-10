165aa488596ef0ec4086365d6c1e81a7da202671
Rob <rob@dijital.ca>
2021-02-09 22:26:01 -0800

    Added RBF and avg settings.

--- src/rastermerge.cpp
+++ src/rastermerge.cpp
@@ -14,6 +14,7 @@
 
 #include <gdal_priv.h>
 
+#include "alglib/interpolation.h"
 #include "grid.hpp"
 #include "util.hpp"
 
@@ -211,10 +212,125 @@ void processIDW(std::list<std::pair<int, int>>* rowq, std::mutex* qmtx,
 	}
 }
 
+/**
+ * Process inverse distance waiting.
+ * \param rowq A queue of row index/block height pairs.
+ * \param qmtx A mutex to protect the queue.
+ * \param src The source (difference) raster.
+ * \param dst The target (output) raster.
+ * \param dmtx A mutex to protect the output raster.
+ * \param size The search radius in pixels.
+ */
+void processRBF(std::list<std::pair<int, int>>* rowq, std::mutex* qmtx,
+		Band<float>* src, Band<float>* dst, std::mutex* dmtx, int size) {
+
+	// The row index and block height.
+	int row, block;
+
+	// The source and destination properties.
+	const GridProps& sprops = src->props();
+	const GridProps& dprops = dst->props();
+
+	// Row data buffer.
+	std::vector<float> rowbuf;
+
+	// Cols and rows in the destination raster.
+	int dcols = dst->props().cols();
+	int drows = dst->props().rows();
+
+	while(!rowq->empty()) {
+		{
+			// Grab a row/block pair off the queue.
+			std::lock_guard<std::mutex> lk(*qmtx);
+			if(!rowq->empty()) {
+				std::pair<int, int>& item = rowq->front();
+				row = item.first;
+				block = item.second;
+				rowq->pop_front();
+				std::cout << "Row " << row << " of " << drows << "\n";
+			} else {
+				std::this_thread::yield();
+				continue;
+			}
+		}
+
+		// If the block size is too large, shrink it.
+		if(row + block >= drows)
+			block = drows - row;
+
+		// Resize and fill the row data buffer.
+		rowbuf.resize(dcols * block);
+		std::fill(rowbuf.begin(), rowbuf.end(), dprops.nodata());
+
+		// Squared radius for validation.
+		float maxrad = size * size;
+		float nearest;
+		// Raster values.
+		float v0, v1;
+		for(int b = 0; b < block; ++b) {
+			for(int col = 0; col < dcols; ++col) {
+				// If the destination pixel is invalid skip it.
+				if((v1 = dst->get(col, row + b)) == dprops.nodata())
+					continue;
+				// The sum and weight variables.
+				float s = 0;
+				float w = 0;
+				// If true, exit the loops.
+				bool halt = false;
+				// Col and row in the difference raster.
+				int scol = sprops.toCol(dprops.toX(col));
+				int srow = sprops.toRow(dprops.toY(row));
+				nearest = geo::maxvalue<float>();
+				// Run the circular kernel.
+				for(int r = -size; !halt && r < size+ 1; ++r) {
+					for(int c = -size; c < size + 1; ++c) {
+						// Calculate the kernel pixel.
+						int cc = scol + c;
+						int rr = srow + r;
+						// The distance from the kernel center.
+						float d = (float) (c * c + r * r);
+						// If the distance is within the max radius, and the pixel is valid, add to sum and weight.
+						if(d <= maxrad
+								&& sprops.hasCell(cc, rr)
+								&& (v0 = src->get(cc, rr)) != sprops.nodata()) {
+							if(false) { //d == 0) {
+								// If the distance is zero, use the value directly and halt.
+								s = v0;
+								w = 1;
+								halt = true;
+								break;
+							} else {
+								// Add to sum and weight.
+								if(d < nearest)
+									nearest = d;
+								float w0 = 1.0f - (d / maxrad);
+								s += v0 * w0;
+								w += w0;
+							}
+						}
+					}
+				}
+				// If the weight is valid, adjust the value.
+				if(w > 0) {
+					rowbuf[b * dcols + col] = v1 + (s / w) * (1.0f - nearest / maxrad);
+				} else {
+					rowbuf[b * dcols + col] = v1;
+				}
+			}
+		}
+
+		// Write the block to the output raster.
+		std::lock_guard<std::mutex> lk(*dmtx);
+		for(int b = 0; b < block; ++b)
+			dst->setRow(row + b, rowbuf.data() + (b * dcols));
+	}
+}
+
 int main(int argc, char** argv) {
 
 	if(argc < 6) {
 		std::cerr << "Usage: rastermerge [options] <<anchor file 1> <anchor band 1> [<anchor file 2> <anchor band 2> [...]]> <target file 2> <target band 2> <output file>\n"
+				<< " -m <method         avg (shift by average difference), idw or rbf.\n"
 				<< " -s <size>          The radius of the window in map units. If a comma-delimited list, will make\n"
 				<< "                    multiple passes.\n"
 				<< " -r <mult>          A resample multiplier. 1 is no change, 2 doubles the side of a pixel, etc.\n"
@@ -236,6 +352,7 @@ int main(int argc, char** argv) {
 	std::string maskfile;
 	int maskband = 0;
 	bool mapped = true;
+	std::string method;
 
 	for(int i = 1; i < argc; ++i) {
 		std::string arg(argv[i]);
@@ -244,6 +361,8 @@ int main(int argc, char** argv) {
 			geo::util::split(std::back_inserter(rad), argv[++i], ",");
 			for(const std::string& r : rad)
 				radii.push_back(atof(r.c_str()));
+		} else if(arg == "-m") {
+			method = argv[++i];
 		} else if(arg == "-r") {
 			std::vector<std::string> res;
 			geo::util::split(std::back_inserter(res), argv[++i], ",");
@@ -274,6 +393,9 @@ int main(int argc, char** argv) {
 		g_runerr("No radii specified. Need at least one.");
 	if(radii.size() != resamples.size())
 		g_runerr("The radius and resample lists should be the same length.");
+
+	if(method != "idw")
+		radii.resize(1);
 
 	g_debug("Using mapped memory: " << mapped);
 
@@ -313,6 +435,8 @@ int main(int argc, char** argv) {
 		mask.init(maskfile, maskband - 1, false, mapped);
 		mprops = mask.props();
 	}
+
+	int valid = 0; //  The number of valid difference pixels.
 
 	for(int i = 0; i < (int)radii.size(); ++i) {
 		float radius = radii[i];
@@ -367,6 +491,7 @@ int main(int argc, char** argv) {
 				if((v = rdiff.get(col1 / resample, row1 / resample)) == rprops.nodata())
 					v = 0;
 				rdiff.set(col1 / resample, row1 / resample, v + v2 - v1);
+				++valid;
 			}
 		}
 
@@ -375,10 +500,13 @@ int main(int argc, char** argv) {
 			g_debug("Normalizing difference raster.")
 			rows = rprops.rows();
 			cols = rprops.cols();
+			valid = 0;
 			for(int row = 0; row < rows; ++row) {
 				for(int col = 0; col < cols; ++col) {
-					if((v = rdiff.get(col, row)) != rprops.nodata())
+					if((v = rdiff.get(col, row)) != rprops.nodata()) {
 						rdiff.set(col, row, v / (resample * resample));
+						++valid;
+					}
 				}
 			}
 		}
@@ -404,8 +532,62 @@ int main(int argc, char** argv) {
 				size++;
 
 			g_debug("Starting processor threads.");
-			for(int i = 0; i < tcount; ++i)
-				threads.emplace_back(processIDW, &rowq, &qmtx, &rdiff, &output, &dmtx, size);
+			if(method == "idw") {
+
+				for(int i = 0; i < tcount; ++i)
+					threads.emplace_back(processIDW, &rowq, &qmtx, &rdiff, &output, &dmtx, size);
+
+			} else if(method == "rbf") {
+
+				alglib::rbfmodel rm;
+				try{
+					std::vector<double> pts(valid * 3);
+					size_t i = 0;
+					double v;
+					for(int r = 0; r < rprops.rows(); ++r) {
+						for(int c = 0; c < rprops.cols(); ++c) {
+							if((v = rdiff.get(c, r)) != rprops.nodata()) {
+								pts[i++] = rprops.toX(c);
+								pts[i++] = rprops.toY(r);
+								pts[i++] = v;
+							}
+						}
+					}
+
+					if(pts.size() > 10000) {
+						unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+						std::shuffle(pts.begin(), pts.end(), std::default_random_engine(seed));
+						pts.resize(10000);
+					}
+
+					alglib::rbfcreate(2, 1, rm);
+					alglib::real_2d_array xy;
+					xy.setcontent(pts.size() / 3, 3, pts.data());
+					alglib::rbfsetpoints(rm, xy, pts.size() / 3);
+					alglib::rbfsetalgohierarchical(rm, 10000, 10, 0.001);
+					alglib::rbfreport rep;
+					alglib::rbfbuildmodel(rm, rep);
+
+				} catch(const std::exception& ex) {
+					g_error(ex.what());
+					return 1;
+				}
+
+				for(int i = 0; i < tcount; ++i)
+					threads.emplace_back(processRBF, &rowq, &qmtx, &rdiff, &output, &dmtx, size);
+
+			} else if(method == "avg") {
+
+				tcount = 0;
+				const GridStats stats = rdiff.stats();
+				for(int r = 0; r < oprops.rows(); ++r) {
+					for(int c = 0; c < oprops.cols(); ++c) {
+						if((v = output.get(c, r)) != oprops.nodata())
+							output.set(c, r, v + stats.mean);
+					}
+				}
+
+			}
 
 			// Wait for threads to complete.
 			for(int i = 0; i < tcount; ++i) {
