4b4f34524529e33cede8256b06d7054fd1d96a74
Rob <rob@dijital.ca>
2021-02-28 22:26:21 -0800

    Upgrades for polygonize.

--- src/polygonize.cpp
+++ src/polygonize.cpp
@@ -8,14 +8,19 @@
 #include <grid.hpp>
 
 void usage() {
-	std::cerr << "Usage: polygonize <input raster> <output vector> [field name (dn)]\n"
+	std::cerr << "Usage: polygonize <input raster> [output vector]\n"
 			<< " -f <format>     The output format. Any of the available\n"
-			<< "                 OGR drivers. Default SQLite.\n"
+			<< "                 OGR drivers. Default SQLite. If PG, Postgres,\n"
+			<< "                 PostgreSQL or PostGIS treats outfile as a connection string.\n"
 			<< " -b <band>       The band. Default 1.\n"
 			<< " -d              Remove dangles.\n"
 			<< " -h              Remove holes.\n"
 			<< " -t <t>          The number of threads. Default 3. There are 2 threads for\n"
 			<< "                 reading and writing and n threads for processing polygons.\n"
+			<< " -l <layer>      The layer name for database formats.\n"
+			<< " -i <field>      The field to store the pixel value (id).\n"
+			<< " -g <geom>       The name of the geometry field (geom).\n"
+			<< " -v <k=v[,k=v]>  A comma-delimited list of fields and values to insert.\n"
 			<< " -m <mask image> A mask image to determine the bounds of the input.\n";
 }
 
@@ -35,11 +40,29 @@ int main(int argc, char** argv) {
 	bool dangles = false;
 	std::vector<std::string> args;
 	std::string mask;
+	std::string layer;
+	std::string idField = "id";
+	std::string geomField = "geom";
+	std::vector<PolygonValue> fieldValues;
 
 	for(int i = 1; i < argc; ++i) {
 		std::string v = argv[i];
 		if(v == "-f") {
 			driver = argv[++i];
+		} else if(v == "-l") {
+			layer = argv[++i];
+		} else if(v == "-i") {
+			idField = argv[++i];
+		} else if(v == "-g") {
+			geomField = argv[++i];
+		} else if(v == "-v") {
+			std::vector<std::string> pairs;
+			std::vector<std::string> kv;
+			geo::util::split(std::back_inserter(pairs), argv[++i], ",");
+			for(const std::string pair : pairs) {
+				geo::util::split(std::back_inserter(kv), pair, "=");
+				fieldValues.emplace_back(kv[0], kv[1]);
+			}
 		} else if(v == "-b") {
 			band = atoi(argv[++i]);
 		} else if(v == "-h") {
@@ -69,12 +92,19 @@ int main(int argc, char** argv) {
 		threads = 1;
 	}
 
+	std::vector<std::string> dbdrivers = {"postgres", "postgresql", "pg", "postgis"};
+	std::string ldriver = geo::util::lowercase(driver);
+
 	std::string infile = args[0];
 	std::string outfile = args[1];
-	std::string layer = args[2];
-	std::string id = "id";
+
 	Band<uint32_t> test(infile, band - 1, false, true);
-	test.polygonizeToFile(outfile, layer, id, driver, holes, dangles);
+
+	if(std::find(dbdrivers.begin(), dbdrivers.end(), ldriver) == dbdrivers.end()) {
+		test.polygonizeToFile(outfile, layer, idField, driver, fieldValues, holes, dangles);
+	} else {
+		test.polygonizeToTable(outfile, layer, idField, geomField, fieldValues, holes, dangles);
+	}
 	return 0;
 }
 
